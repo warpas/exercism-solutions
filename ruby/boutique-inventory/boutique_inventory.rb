a6cf3935d51f3aa60cd062204f286354a47a9642
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-11-28 22:16:45 +0100

    Add Ruby track boutique-inventory solution

---
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -0,0 +1,30 @@
+class BoutiqueInventory
+  CHEAP_ITEM_THRESHOLD = 30
+
+  def initialize(items)
+    @items = items
+  end
+
+  def item_names
+    items.map { |item| item[:name] }.sort
+  end
+
+  def cheap
+    items.select { |item| item[:price] < CHEAP_ITEM_THRESHOLD }
+  end
+
+  def out_of_stock
+    items.select { |item| item[:quantity_by_size].empty? }
+  end
+
+  def stock_for_item(name)
+    items.find { |item| item[:name] == name }[:quantity_by_size]
+  end
+
+  def total_stock
+    items.map { |item| item[:quantity_by_size].values.sum }.sum
+  end
+
+  private
+  attr_reader :items
+end
