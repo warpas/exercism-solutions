e38347a1f03393791cf1cebd9dc7586749858ca9
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-11-28 18:59:31 +0100

    Ruby track solutions submitted on NOV 25

---
+++ ruby/bird-count/bird_count.rb
@@ -0,0 +1,25 @@
+class BirdCount
+  def self.last_week
+    [0, 2, 5, 3, 7, 8, 4]
+  end
+
+  def initialize(birds_per_day)
+    @birds_per_day = birds_per_day
+  end
+
+  def yesterday
+    @birds_per_day[-2]
+  end
+
+  def total
+    @birds_per_day.reduce(:+)
+  end
+
+  def busy_days
+    @birds_per_day.count { |d| d > 4 }
+  end
+
+  def day_without_birds?
+    !@birds_per_day.select { |d| d == 0 }.empty?
+  end
+end
