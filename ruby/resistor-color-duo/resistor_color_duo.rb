1e7b2fc6e3aabafa8f4a87a63b44ce56df7ca3e5
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-12-02 09:00:01 +0100

    Add Ruby track solution for resistor-color-duo iteration 2

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,19 +1,7 @@
 class ResistorColorDuo
-  CODES =
-    {
-      black: 0,
-      brown: 1,
-      red: 2,
-      orange: 3,
-      yellow: 4,
-      green: 5,
-      blue: 6,
-      violet: 7,
-      grey: 8,
-      white: 9
-    }.freeze
+  CODES = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.value(colors)
-    10 * CODES[colors[0].to_sym] + CODES[colors[1].to_sym]
+    colors.take(2).map { |c| CODES.index(c) }.join.to_i
   end
 end
