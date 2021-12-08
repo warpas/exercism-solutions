0f6eec970f1526fdda58f6b83566c0bf2d0b5c3c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-12-08 08:53:59 +0100

    Add Ruby track solution for isogram iteration#2

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -2,11 +2,7 @@ class Isogram
   ALLOWED_DUPLICATES = ['-', ' '].freeze
 
   def self.isogram?(string)
-    hash = Hash.new(0)
-    string.downcase.chars do |letter|
-      hash[letter] += 1 unless ALLOWED_DUPLICATES.include? letter
-      return false if hash[letter] > 1
-    end
-    true
+    letters = string.downcase.chars - ALLOWED_DUPLICATES
+    letters == letters.uniq
   end
 end
