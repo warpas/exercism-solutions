3f8d423bc03d1a781bafc10dea24e537d907c2af
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-12-28 16:22:43 +0100

    Add Ruby track solution for gigasecond iteration #2

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -2,7 +2,10 @@
 
 # Implementation of the Gigasecond exercise in Ruby track on Exercism.
 class Gigasecond
+
+  # @param time [Time]
+  # @return [Time] time after 1_000_000_000 seconds
   def self.from(time)
-    Time.at(time.to_i + 1_000_000_000)
+    time + 1e9
   end
 end
