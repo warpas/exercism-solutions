cd125a5e4bcf0c1ac9be45c50291258fa87cf916
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2021-12-29 16:18:05 +0100

    Add Ruby track solution for microwave iteration #2

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -2,33 +2,17 @@
 
 # Implementation of the Microwave exercise in Ruby track on Exercism.
 class Microwave
-
+  TIMER_FORMAT = '%<min>02d:%<sec>02d'
   # @param [Integer] time in microwave timer notation
   # @return [Integer] seconds from the microwave timer
   def initialize(time_notation)
-    @minutes = time_notation / 100
-    @seconds = time_notation % 100
+    @minutes = (time_notation / 100) + (time_notation % 100 / 60)
+    @seconds = (time_notation % 100) % 60
   end
 
   # @param [void]
   # @return [String] timer formatted to mm:ss
   def timer
-    "#{normalized_minutes}:#{normalized_seconds}"
-  end
-
-  private
-
-  def normalized_seconds
-    ts = @seconds % 60
-    return ts if ts > 9
-
-    "0#{ts}"
-  end
-
-  def normalized_minutes
-    tm = @minutes + @seconds / 60
-    return tm if tm > 9
-
-    "0#{tm}"
+    format(TIMER_FORMAT, { min: @minutes, sec: @seconds })
   end
 end
