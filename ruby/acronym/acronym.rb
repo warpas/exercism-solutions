c64db1ff3aadc42a9dba6b6b50f4d3d2a9bfef29
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-01 20:11:01 +0100

    Add comments to Acronym solution

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+# Implementation of the Acronym exercise in Ruby track on Exercism.
 class Acronym
+  # @param [String] sentence
+  # @return [String] acronym of the input
   def self.abbreviate(string)
     string.split(/[-\s]/).map(&:chr).join.upcase
   end
