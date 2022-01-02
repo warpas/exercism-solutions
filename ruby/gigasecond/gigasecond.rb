24de6b712bda74aef73baf56b8136bae9a3a7f6d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-02 16:21:28 +0100

    Add comments to Gigasecond solution

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -2,8 +2,7 @@
 
 # Implementation of the Gigasecond exercise in Ruby track on Exercism.
 class Gigasecond
-
-  # @param time [Time]
+  # @param time [Time] Ruby Time instance
   # @return [Time] time after 1_000_000_000 seconds
   def self.from(time)
     time + 1e9
