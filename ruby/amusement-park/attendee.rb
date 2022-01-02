60b1d7426ffae20ab3c64c5fdc0033b5e7e45af2
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-02 16:19:43 +0100

    Add comments to Amusement Park solution

--- ruby/amusement-park/attendee.rb
+++ ruby/amusement-park/attendee.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
+# Implementation of the Amusement Park exercise in Ruby track on Exercism.
 class Attendee
+  attr_reader :height, :pass_id
+
+  # @param height [Integer] in centimeters
+  # @return [Attendee] attendee without a pass
   def initialize(height)
     @height = height
+    revoke_pass!
   end
 
-  def height
-    @height
-  end
-
-  def pass_id
-    return nil unless @pass_id
-
-    @pass_id
-  end
-
+  # @param pass_id [Integer] pass ID number
+  # @return [Integer] new pass ID
   def issue_pass!(pass_id)
     @pass_id = pass_id
   end
 
+  # @return [nil] nil represents missing pass
   def revoke_pass!
     @pass_id = nil
   end
