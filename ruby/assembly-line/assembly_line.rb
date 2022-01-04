891dde42c9953f20ab6b6cd7fddccb266f2b155d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-04 08:36:20 +0100

    Add comments to Assembly Line solution

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,14 +1,20 @@
+# frozen_string_literal: true
+
+# Implementation of the Assembly Line exercise in Ruby track on Exercism.
 class AssemblyLine
+  # Number of cars produced at 1x speed
   FLAWLESS_RATE = 221
 
   def initialize(speed)
     @speed = speed
   end
 
+  # @return [Float] production rate per hour that takes success rate into account
   def production_rate_per_hour
-    @speed * FLAWLESS_RATE * success_rate
+    (@speed * FLAWLESS_RATE * success_rate).to_f
   end
 
+  # @return [Integer] number of successfully produced items per minute
   def working_items_per_minute
     (production_rate_per_hour / 60).to_i
   end
