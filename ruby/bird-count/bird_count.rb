899744a9f87d1bb586bf2ca904df1e01d79941c6
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-05 17:25:49 +0100

    Add comments to Bird Count solution

--- ruby/bird-count/bird_count.rb
+++ ruby/bird-count/bird_count.rb
@@ -1,27 +1,33 @@
 # frozen_string_literal: true
 
-# Implementation of the BirdCount exercise in Ruby track on Exercism.
+# Implementation of the Bird Count exercise in Ruby track on Exercism.
 class BirdCount
+  # @return [Array<Integer>] last week's bird counts
   def self.last_week
     [0, 2, 5, 3, 7, 8, 4]
   end
 
+  # @param birds_per_day [Array<Integer>] this week's bird counts
   def initialize(birds_per_day)
     @birds_per_day = birds_per_day
   end
 
+  # @return [Integer] yesterday's result
   def yesterday
     @birds_per_day[-2]
   end
 
+  # @return [Integer] this week's total bird count
   def total
     @birds_per_day.reduce(:+)
   end
 
+  # @return [Integer] the number of this week's busy days
   def busy_days
     @birds_per_day.count { |d| d > 4 }
   end
 
+  # @return [Boolean] did this week have any empty days?
   def day_without_birds?
     !@birds_per_day.select(&:zero?).empty?
   end
