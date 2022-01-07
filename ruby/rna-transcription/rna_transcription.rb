8f6dd96a2517a37cff1aa81d99e128d01bd76e6c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-07 16:01:23 +0100

    Hide private constant from the public interface in RNA Transcription solution

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-# Implementation of the Complement exercise in Ruby track on Exercism.
+# Implementation of the RNA Transcription exercise in Ruby track on Exercism.
 class Complement
-  RNA = 'CGAU' # translation sequence for RNA
-  DNA = 'GCTA' # translation sequence for DNA
-
   # @param strand [String] DNA strand
   # @return [String] transcribed RNA strand
   def self.of_dna(strand)
     strand.tr(DNA, RNA)
   end
+
+  RNA = 'CGAU'
+  DNA = 'GCTA'
+  private_constant :RNA
+  private_constant :DNA
 end
