dbd7c4e49c44f233b060ec9b3d578e75d2d9f973
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-07 16:23:04 +0100

    Add comments to Protein Translation solution

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
+# Error type for invalid Codons considered for translation
 class InvalidCodonError < RuntimeError
 end
 
 # Implementation of the Translation exercise in Ruby track on Exercism.
 class Translation
   class << self
+    # @param codon [String] RNA broken into three nucleotide sequences
+    # @return [String] sequences transcribed to a protein
     def of_codon(codon)
       protein = CODONS[codon.to_sym]
       protein || raise(InvalidCodonError)
     end
 
+    # @param strand [String] RNA strand
+    # @return [String] transcribed list of proteins
     def of_rna(strand)
       strand.scan(/.../).reduce([]) do |chain, codon|
         protein = of_codon(codon)
