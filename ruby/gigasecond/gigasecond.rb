8e88afb6ae2835042dadf34d23cf5945a2ed223d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-07 15:55:10 +0100

    Move private definitions to the bottom in Gigasecond solution

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -2,12 +2,12 @@
 
 # Implementation of the Gigasecond exercise in Ruby track on Exercism.
 class Gigasecond
-  GIGASECOND = 1e9
-  private_constant :GIGASECOND
-
   # @param time [Time] Ruby Time instance
   # @return [Time] time after 1_000_000_000 seconds
   def self.from(time)
     time + GIGASECOND
   end
+
+  GIGASECOND = 1e9
+  private_constant :GIGASECOND
 end
