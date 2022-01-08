0ef562cd26418950f86bee574aacf72e2da67a68
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-08 18:17:36 +0100

    Add Ruby track solution for resistor_color iteration#2

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
-# Implementation of the ResistorColor exercise in Ruby track on Exercism.
+# Implementation of the Resistor Color exercise in Ruby track on Exercism.
 class ResistorColor
+  # Resistor color bands sorted by resistance values
   COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
+  # @param color [String] resistor band color
+  # @return [Integer] the resistance value for the given color
   def self.color_code(color)
     COLORS.index(color)
   end
