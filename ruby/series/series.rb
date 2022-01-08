f7e9690ff000b76d692918e0277d10fa4785b01c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-08 18:47:32 +0100

    Add comments to Series solution

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,8 +1,15 @@
+# frozen_string_literal: true
+
+# Implementation of the Series exercise in Ruby track on Exercism.
 class Series
+  # @param string [String]
   def initialize(string)
     @series = string
   end
 
+  # @param substring_length [Integer]
+  # @raise [ArgumentError] if the desired substring length is longer than the string under consideration
+  # @return [Array<String>] list of all substrings of the given length
   def slices(substring_length)
     valid_substring_length = @series.length - substring_length
     raise ArgumentError if valid_substring_length.negative?
