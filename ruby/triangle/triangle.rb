b4293b8768baa79af51c313324654d3cfe7728b2
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-08 17:53:13 +0100

    Add Ruby track solution for Triangle iteration #2

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -5,59 +5,31 @@ class Triangle
   # @param sides [Array] a list of side lengths of a triangle
   def initialize(sides)
     @sides = sides
-    minmax = sides.minmax
-    @shortest_side = minmax.first
-    @longest_side = minmax.last
+    @distinct_side_lengths = sides.uniq.count
+    @is_proper_triangle = triangle?
   end
 
   # @return [Boolean] is the triangle equilateral?
   def equilateral?
-    triangle? && all_sides_equal?
+    is_proper_triangle && distinct_side_lengths == 1
   end
 
   # @return [Boolean] is the triangle isoscales?
   def isosceles?
-    triangle? && any_two_sides_equal?
+    is_proper_triangle && distinct_side_lengths <= 2
   end
 
   # @return [Boolean] is the triangle scalene?
   def scalene?
-    triangle? && no_two_sides_equal?
+    is_proper_triangle && distinct_side_lengths == 3
   end
 
   private
 
-  attr_reader :longest_side, :shortest_side
-
-  def side1_eq_side2?
-    @sides[0] == @sides[1]
-  end
-
-  def side1_eq_side3?
-    @sides[0] == @sides[2]
-  end
-
-  def side2_eq_side3?
-    @sides[1] == @sides[2]
-  end
-
-  def any_two_sides_equal?
-    side1_eq_side2? || side1_eq_side3? || side2_eq_side3?
-  end
-
-  def no_two_sides_equal?
-    !any_two_sides_equal?
-  end
-
-  def all_sides_equal?
-    shortest_side == longest_side
-  end
-
-  def perimiter_longer_that_twice_the_longest_side?
-    @sides.sum >= longest_side * 2
-  end
+  attr_reader :is_proper_triangle, :distinct_side_lengths
 
   def triangle?
-    shortest_side.positive? && perimiter_longer_that_twice_the_longest_side?
+    shortest_side, longest_side = @sides.minmax
+    shortest_side.positive? && @sides.sum >= longest_side * 2
   end
 end
