09daeb353773ce4a0fa871d11f6bd819b2c8bd09
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-08 16:43:47 +0100

    Add comments to Word Count solution

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
+# Implementation of the Word Count exercise in Ruby track on Exercism.
 class Phrase
+  # @param phrase [String]
   def initialize(phrase)
     @words = phrase.downcase.scan(/\b[\w']+\b/)
   end
 
+  # @return [Hash<String, Integer>] Keys represent words from the inital phrase,
+  #   values track the amount of times that word occurred
   def word_count
     @words.each_with_object(Hash.new(0)) do |word, hash|
       hash[word] += 1
