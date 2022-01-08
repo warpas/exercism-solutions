634c3ac4662a3e193e174c9d58b176675b12fa64
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-08 19:46:34 +0100

    Add comments to Hamming solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,11 +1,18 @@
-class Hamming
-  def self.compute(strand, strand2)
-    raise ArgumentError if strand.length != strand2.length
+# frozen_string_literal: true
+
+# Implementation of the Hamming exercise in Ruby track on Exercism.
+module Hamming
+  # @param strand1 [String] first DNA strand
+  # @param strand2 [String] second DNA strand
+  # @raise [ArgumentError] if strands are of unequal length
+  # @return [Integer] Hamming Distance between the two DNA strands
+  def self.compute(strand1, strand2)
+    raise ArgumentError if strand1.length != strand2.length
 
     differences = 0
-    strand1_array = strand.chars
+    strand1_array = strand1.chars
     strand2_array = strand2.chars
-    0.upto(strand.length - 1) do |index|
+    0.upto(strand1.length - 1) do |index|
       differences += 1 if strand1_array[index] != strand2_array[index]
     end
     differences
