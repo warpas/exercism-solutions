d465e296a3863c642e64271b0d14f433b3a3b805
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-08 19:01:18 +0100

    Alter Error definition to oneline in Protein Translation solution

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 # Error type for invalid Codons considered for translation
-class InvalidCodonError < RuntimeError
-end
+class InvalidCodonError < RuntimeError; end
 
 # Implementation of the Translation exercise in Ruby track on Exercism.
 class Translation
