63c98b3689c0b744031165b1770faeb54676531c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-09 20:37:04 +0100

    Add comments to Leap solution

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -2,7 +2,9 @@
 
 # Implementation of the Leap exercise in Ruby track on Exercism.
 class Year
+  # @param year [Integer]
+  # @return [Boolean] is the year a leap year?
   def self.leap?(year)
-    (year % 4).zero? && (year % 100 != 0 || year % 400.zero?)
+    (year % 4).zero? && ((year % 100).positive? || (year % 400).zero?)
   end
 end
