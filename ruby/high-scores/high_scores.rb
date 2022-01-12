369026ac3a079f27bb2d1393d74d170d4c7da7aa
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-12 08:30:26 +0100

    Add comments to High Scores solution

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,23 +1,31 @@
+# frozen_string_literal: true
+
+# Implementation of the High Scores exercise in Ruby track on Exercism.
 class HighScores
   attr_reader :scores
 
+  # @param scores [Array<Integer>] collection of scores from the game
   def initialize(scores)
     @scores = scores
     @sorted_scores = scores.sort
   end
 
+  # @return [Integer] the score from the last game
   def latest
     scores.last
   end
 
+  # @return [Integer] the best score recorded in the game
   def personal_best
     sorted_scores.last
   end
 
+  # @return [Array<Integer>] collection of the three best scores from the game
   def personal_top_three
     sorted_scores.last(3).reverse
   end
 
+  # @return [Boolean] is the last score from the last game also the personal best?
   def latest_is_personal_best?
     latest == personal_best
   end
