e1ca62b3816438318a64df36929904a1b92dcc4f
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-16 21:17:35 +0100

    Add Elixir track solution for Lasagna iteration#2

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -2,22 +2,22 @@ defmodule Lasagna do
   @moduledoc """
   Implementation of the Lasagna exercise in Elixir track on Exercism.
   """
-  # Please define the 'expected_minutes_in_oven/0' function
+  @spec expected_minutes_in_oven :: Integer.t()
   def expected_minutes_in_oven do
     40
   end
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  @spec remaining_minutes_in_oven(Integer.t()) :: Integer.t()
   def remaining_minutes_in_oven(elapsed) do
     expected_minutes_in_oven() - elapsed
   end
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  @spec preparation_time_in_minutes(Integer.t()) :: Integer.t()
   def preparation_time_in_minutes(layers) do
     layers * 2
   end
 
-  # Please define the 'total_time_in_minutes/2' function
+  @spec total_time_in_minutes(Integer.t(), Integer.t()) :: Integer.t()
   def total_time_in_minutes(layers, minutes_in_oven) do
     preparation_time_in_minutes(layers) + minutes_in_oven
   end
