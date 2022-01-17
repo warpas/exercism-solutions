5ed3b080f5b23acbf76f5a72571c283f591e217c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-17 08:39:05 +0100

    Privatize constants in Space Age solution

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -3,6 +3,8 @@
 # Implementation of the SpaceAge exercise in Ruby track on Exercism.
 class SpaceAge
   EARTH_YEAR_IN_SECONDS = 31_557_600
+  private_constant :EARTH_YEAR_IN_SECONDS
+
   PLANET_YEAR_TO_EARTH_RATIO = {
     earth: 1,
     mercury: 0.2408467,
@@ -13,6 +15,7 @@ class SpaceAge
     uranus: 84.016846,
     neptune: 164.79132
   }.freeze
+  private_constant :PLANET_YEAR_TO_EARTH_RATIO
 
   def initialize(seconds)
     @seconds = seconds
