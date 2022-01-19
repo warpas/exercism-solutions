f11e5c0a915c7530ce4223762be1d3e8a2c62241
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-19 22:25:10 +0100

    Add comments to Log Line Parser solution

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -1,16 +1,23 @@
+# frozen_string_literal: true
+
+# Implementation of the Log Line Parser exercise in Ruby track on Exercism.
 class LogLineParser
+  # @param line [Integer] log entry consisting of log level and message
   def initialize(line)
     @line = line
   end
 
+  # @return [String] log message
   def message
     @line.split(':').last.strip
   end
 
+  # @return [String] severity of the message
   def log_level
     @line.split(':').first[1..-2].downcase
   end
 
+  # @return [String] reformatted log message with log level at the end
   def reformat
     "#{message.capitalize} (#{log_level})"
   end
