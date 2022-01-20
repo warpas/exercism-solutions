1471f2a385acf789694de165a04ebed85972165a
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-20 09:02:38 +0100

    Add comments to Luhn solution

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
-# Implementation of the Luhn algorithm in Ruby track on Exercism.
+# Implementation of the Luhn exercise in Ruby track on Exercism.
 class Luhn
+  # @param arg [String] identification number to be validated
+  # @return [Boolean] is it valid by the standards of the Luhn algorithm?
   def self.valid?(arg)
     new(arg).valid?
   end
 
+  # @param string [String]
   def initialize(string)
     @string = string.strip
   end
 
+  # @return [Boolean] is it valid by the standards of the Luhn algorithm?
   def valid?
     return false if @string.length < 2
     return false unless valid_chars_only?
 
     (sum_up_luhn % 10).zero?
   end
+
+  private
 
   def sum_up_luhn
     every_other_starting_at0 = []
@@ -32,8 +38,6 @@ class Luhn
 
     every_other_starting_at0.sum + every_other_starting_at1.sum
   end
-
-  private
 
   def valid_chars_only?
     @string.scan(/\D/).reject { |nondigit| nondigit == ' ' }.empty?
