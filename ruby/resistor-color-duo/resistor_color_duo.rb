2a898720dc72f0acdee05db356d00a77381dc86c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-22 12:51:17 +0100

    Add comments to Resistor Color Duo solution

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
+# Implementation of the Resistor Color Duo exercise in Ruby track on Exercism.
 class ResistorColorDuo
+  # Resistor color bands sorted by resistance values
   CODES = %w[black brown red orange yellow green blue violet grey white].freeze
 
+  # @param colors [Array<String>] resistor band colors
+  # @return [Integer] the resistance value for the given pair of colors
   def self.value(colors)
     colors.take(2).map { |c| CODES.index(c) }.join.to_i
   end
