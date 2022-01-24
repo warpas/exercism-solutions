622f5c8f6b07bf9cdf9779b925bd0a850d96fdab
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-24 10:19:35 +0100

    Add comments to Matrix solution

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
+# Implementation of the Matrix exercise in Ruby track on Exercism.
 class Matrix
   attr_reader :rows, :columns
 
+  # @param string [String]
   def initialize(string)
     @rows = string.each_line.map { |line| line.split.map(&:to_i) }
     @columns = rows.transpose
