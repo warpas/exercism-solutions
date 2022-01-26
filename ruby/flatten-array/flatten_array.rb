c260c631abd99daf6235556fcf6d2073aebcbbd5
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-26 22:53:30 +0100

    Add Ruby track solution for Flatten Array iteration#4

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -2,16 +2,19 @@
 
 # Implementation of the Flatten Array exercise in Ruby track on Exercism. Missing the point of the exercise!
 class FlattenArray
-  # @param array [Array<Integer, nil>]
-  # @return [Array<Integer>]
-  def self.flatten(array)
-    recursive_flatten(array).reject(&:nil?)
-  end
-
-  def self.recursive_flatten(array)
-    array.reduce([]) do |acc, elem|
-      elem.instance_of?(Array) ? acc.concat(recursive_flatten(elem)) : acc.push(elem)
+  class << self
+    # @param array [Array<Integer, nil>]
+    # @return [Array<Integer>]
+    def flatten(array)
+      recursive_flatten(array).reject(&:nil?)
     end
+
+    def recursive_flatten(array)
+      array.reduce([]) do |acc, elem|
+        elem.instance_of?(Array) ? acc.concat(recursive_flatten(elem)) : acc.push(elem)
+      end
+    end
+
   end
 
   private_class_method :recursive_flatten
