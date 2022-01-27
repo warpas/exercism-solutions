b67da763a3f03516755325a3b062eb64525bae25
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-27 19:51:28 +0100

    Add Ruby track solution for Phone Number iteration#1

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,7 +1,49 @@
-=begin
-Write your code for the 'Phone Number' exercise in this file. Make the tests in
-`phone_number_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/phone-number` directory.
-=end
+# Implementation of the Phone Number exercise in Ruby track on Exercism.
+class PhoneNumber
+  # @param phone_number [String]
+  # @return [String, nil]
+  def self.clean(phone_number)
+    bare_number = phone_number.scan(/\d+/).join
+
+    return nil if shorter_than_10_digits(bare_number)
+    return nil if longer_than_10_digits_bad_start(bare_number)
+
+    bare_number = bare_number[1..10] if longer_than_10_digits_good_start(bare_number)
+    return nil if area_code_starts_with_zero(bare_number)
+    return nil if area_code_starts_with_one(bare_number)
+    return nil if exchange_code_starts_with_zero(bare_number)
+    return nil if exchange_code_starts_with_one(bare_number)
+
+    bare_number
+  end
+
+  def self.shorter_than_10_digits(bare_number)
+    bare_number.length < 10
+  end
+
+  def self.longer_than_10_digits_bad_start(bare_number)
+    bare_number.length > 10 && bare_number[0] != '1'
+  end
+
+  def self.longer_than_10_digits_good_start(bare_number)
+    bare_number.length > 10 && bare_number[0] == '1'
+  end
+
+  def self.area_code_starts_with_zero(bare_number)
+    bare_number.length == 10 && bare_number[0] == '0'
+  end
+
+  def self.area_code_starts_with_one(bare_number)
+    bare_number.length == 10 && bare_number[0] == '1'
+  end
+
+  def self.exchange_code_starts_with_zero(bare_number)
+    bare_number.length == 10 && bare_number[3] == '0'
+  end
+
+  def self.exchange_code_starts_with_one(bare_number)
+    bare_number.length == 10 && bare_number[3] == '1'
+  end
+end
