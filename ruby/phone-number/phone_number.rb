ebee39127b97aa57ebe6815c92c9c925c5fff03c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-01-30 10:18:50 +0100

    Add Ruby track solution for Phone Number iteration#3

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,27 +2,41 @@
 
 # Implementation of the Phone Number exercise in Ruby track on Exercism.
 class PhoneNumber
-  # @param phone_number [String]
+  # @param raw_phone_number [String]
   # @return [String, nil]
-  def self.clean(phone_number)
-    bare_number = phone_number.scan(/\d+/).join
-    bare_number = bare_number[1..10] if longer_than_10_digits_good_start(bare_number)
-    return nil unless valid_number?(bare_number)
+  def self.clean(raw_phone_number)
+    phone_number = new(raw_phone_number)
 
-    bare_number
+    phone_number.validate
   end
 
-  def self.bad_code(code)
+  # @return [String, nil]
+  def validate
+    return nil unless valid?
+
+    number
+  end
+
+  private
+
+  attr_reader :number
+
+  def valid?
+    area_code = number[0]
+    exchange_code = number[3]
+    number.length == 10 && !bad_code(area_code) && !bad_code(exchange_code)
+  end
+
+  def initialize(raw_number)
+    @number = raw_number.scan(/\d+/).join
+    @number = @number[1..10] if longer_than_10_digits_good_start
+  end
+
+  def longer_than_10_digits_good_start
+    number.length > 10 && number[0] == '1'
+  end
+
+  def bad_code(code)
     %w[0 1].include?(code)
-  end
-
-  def self.valid_number?(phone_number)
-    area_code = phone_number[0]
-    exchange_code = phone_number[3]
-    phone_number.length == 10 && !bad_code(area_code) && !bad_code(exchange_code)
-  end
-
-  def self.longer_than_10_digits_good_start(bare_number)
-    bare_number.length > 10 && bare_number[0] == '1'
   end
 end
