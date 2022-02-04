ae6f239a0229217625af9e9ed3316822aad53122
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-02-04 08:30:37 +0100

    Add comments to Savings Account solution

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,23 +1,36 @@
+# frozen_string_literal: true
+
+# Implementation of the Savings Account exercise in Ruby track on Exercism.
 module SavingsAccount
-  def self.interest_rate(balance)
-    return -3.213 if balance.negative?
-    return 0.5 if balance < 1000
-    return 1.621 if balance < 5000
+  class << self
+    # @param balance [Float]
+    # @return [Float]
+    def interest_rate(balance)
+      return -3.213 if balance.negative?
+      return 0.5 if balance < 1000
+      return 1.621 if balance < 5000
 
-    2.475
-  end
-
-  def self.annual_balance_update(balance)
-    interest = balance.abs * interest_rate(balance) * 0.01
-    balance + interest
-  end
-
-  def self.years_before_desired_balance(current_balance, desired_balance)
-    years = 0
-    while desired_balance > current_balance
-      years += 1
-      current_balance = annual_balance_update(current_balance)
+      2.475
     end
-    years
+
+    # @param balance [Float]
+    # @return [Float]
+    def annual_balance_update(balance)
+      interest = balance.abs * interest_rate(balance) * 0.01
+      balance + interest
+    end
+
+    # @param current_balance [Float]
+    # @param desired_balance [Float]
+    # @return [Integer] years it would take for current balance to accumulate enough interest to reach desired balance
+    def years_before_desired_balance(current_balance, desired_balance)
+      years = 0
+      while desired_balance > current_balance
+        years += 1
+        current_balance = annual_balance_update(current_balance)
+      end
+      years
+    end
   end
+
 end
