a0682173f813763ef53211061c535ebd6f57f103
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-02-10 22:52:17 +0100

    Add comments to Accumulate solution

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -2,6 +2,8 @@
 
 # Implementation of the Accumulate exercise in Ruby track on Exercism.
 class Array
+  # @param block [Block]
+  # @return [Array]
   def accumulate(&block)
     new_array = []
     each do |n|
