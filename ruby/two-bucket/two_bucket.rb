312f3014b912c323c5a638a9d16ce783006621a7
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-02-17 23:10:26 +0100

    Add Ruby track exercise Two Bucket

---
+++ ruby/two-bucket/two_bucket.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Two Bucket' exercise in this file. Make the tests in
+`two_bucket_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/two-bucket` directory.
+=end
