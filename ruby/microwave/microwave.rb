a300c44aa93b18a0dacb9348af7861c60a990c3d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-02-24 08:37:26 +0100

    Add comments to Microwave solution

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -2,15 +2,16 @@
 
 # Implementation of the Microwave exercise in Ruby track on Exercism.
 class Microwave
+  # The format in which Microwave displays time
   TIMER_FORMAT = '%<min>02d:%<sec>02d'
-  # @param [Integer] time in microwave timer notation
+
+  # @param time_notation [Integer] time in microwave timer notation
   # @return [Integer] seconds from the microwave timer
   def initialize(time_notation)
     @minutes = (time_notation / 100) + (time_notation % 100 / 60)
     @seconds = (time_notation % 100) % 60
   end
 
-  # @param [void]
   # @return [String] timer formatted to mm:ss
   def timer
     format(TIMER_FORMAT, { min: @minutes, sec: @seconds })
