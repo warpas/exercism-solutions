8d17baf9e4394b767b6bd320e3c72330ef07a73d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-02-26 16:13:42 +0100

    Add Ruby track solution for Nth Prime iteration#6

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -6,24 +6,27 @@ class Prime
   # @return [Integer]
   def self.nth(index)
     raise ArgumentError if index <= 0
-    return 3 if index == 2
 
     primes = [2]
     candidate = 3
     while index > primes.length
-      viable = true
-      sqrt_of_candidate = Math.sqrt candidate
-      primes.each do |divisor|
-        break if divisor > sqrt_of_candidate
-
-        if (candidate % divisor).zero?
-          viable = false
-          break
-        end
-      end
-      primes.push(candidate) if viable
-      candidate += 1
+      primes << candidate if viable_candidate?(primes, candidate)
+      candidate += 2
     end
     primes[index - 1]
   end
+
+  def self.viable_candidate?(primes, candidate)
+    upper_limit = Math.sqrt candidate
+
+    is_viable = true
+    primes.each do |divisor|
+      break if divisor > upper_limit
+      return false if (candidate % divisor).zero?
+    end
+
+    is_viable
+  end
+
+  private_class_method :viable_candidate?
 end
