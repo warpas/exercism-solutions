55a115a438128040a1cfefd5c7d54304585e6045
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-03-26 22:35:46 +0100

    Add Bash track solution for Two Fer iteration#2 - within main function

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,6 +1,10 @@
-if [ "$1" = "" ]
-then
-  echo "One for you, one for me."
-else
-  echo "One for $1, one for me."
-fi
+main () {
+  if [ "$1" = "" ]
+  then
+    echo "One for you, one for me."
+  else
+    echo "One for $1, one for me."
+  fi
+}
+
+main "$@"
