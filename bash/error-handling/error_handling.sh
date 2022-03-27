0d58e7a79d4f98d7721b800af60f8a569fc83d53
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-03-27 11:27:08 +0200

    Add Bash track solution for Error Handling iteration#2 - wrap into a function with a defined local variable

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,6 +1,11 @@
-if ! [[ $# == 1 ]] ; then
-  echo "Usage: error_handling.sh <person>"
-  exit 1
-fi
+main() {
+  local name="$1"
 
-echo "Hello, $1"
+  if ! [[ $# == 1 ]] ; then
+    echo "Usage: error_handling.sh <person>"
+    exit 1
+  fi
+  echo "Hello, ${name}"
+}
+
+main "$@"
