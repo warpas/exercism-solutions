bd17720d5429e819545654234bbbf23b731f1dd8
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-03-27 11:15:23 +0200

    Add Bash track solution for Error Handling iteration#1

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,24 +1,6 @@
-#!/usr/bin/env bash
+if ! [[ $# == 1 ]] ; then
+  echo "Usage: error_handling.sh <person>"
+  exit 1
+fi
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+echo "Hello, $1"
