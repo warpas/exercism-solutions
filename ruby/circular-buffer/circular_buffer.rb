84dc913e9b14ebd352e86177dc0de7e85fcc92b1
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-04-07 22:10:41 +0200

    Add Ruby track solution for Circular Buffer iteration#1

--- ruby/circular-buffer/circular_buffer.rb
+++ ruby/circular-buffer/circular_buffer.rb
@@ -1,7 +1,49 @@
-=begin
-Write your code for the 'Circular Buffer' exercise in this file. Make the tests in
-`circular_buffer_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/circular-buffer` directory.
-=end
+# Implementation of the Circular Buffer exercise in Ruby track on Exercism.
+class CircularBuffer
+  # Error type for a read from an empty Buffer
+  class BufferEmptyException < RuntimeError; end
+  # Error type for a write to a full Buffer
+  class BufferFullException < RuntimeError; end
+
+  # @param size [Integer]
+  def initialize(size)
+    @size = size
+    clear
+  end
+
+  # @return [String]
+  def read
+    raise BufferEmptyException if @buffer.empty?
+
+    @buffer.shift
+  end
+
+  # @param char [String]
+  # @return [Array]
+  def write(char)
+    raise BufferFullException if buffer_full?
+
+    @buffer << char
+  end
+
+  # @param char [String]
+  # @return [Array]
+  def write!(char)
+    @buffer.shift if buffer_full?
+
+    @buffer << char
+  end
+
+  # @return [Array]
+  def clear
+    @buffer = []
+  end
+
+  private
+
+  def buffer_full?
+    @buffer.length >= @size
+  end
+end
--- ruby/circular-buffer/circular_buffer_test.rb
+++ ruby/circular-buffer/circular_buffer_test.rb
@@ -8,7 +8,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_write_and_read_back_one_item
-    skip
     buffer = CircularBuffer.new(1)
     buffer.write '1'
     assert_equal '1', buffer.read
@@ -16,7 +15,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_write_and_read_back_multiple_items
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write '2'
@@ -26,7 +24,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_clearing_buffer
-    skip
     buffer = CircularBuffer.new(3)
     ('1'..'3').each { |i| buffer.write i }
     buffer.clear
@@ -39,7 +36,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_alternate_write_and_read
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     assert_equal '1', buffer.read
@@ -48,7 +44,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_reads_back_oldest_item
-    skip
     buffer = CircularBuffer.new(3)
     buffer.write '1'
     buffer.write '2'
@@ -59,7 +54,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_writing_to_a_full_buffer_throws_an_exception
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write '2'
@@ -67,7 +61,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_overwriting_oldest_item_in_a_full_buffer
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write '2'
@@ -78,7 +71,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_forced_writes_to_non_full_buffer_should_behave_like_writes
-    skip
     buffer = CircularBuffer.new(2)
     buffer.write '1'
     buffer.write! '2'
@@ -88,7 +80,6 @@ class CircularBufferTest < Minitest::Test
   end
 
   def test_alternate_read_and_write_into_buffer_overflow
-    skip
     buffer = CircularBuffer.new(5)
     ('1'..'3').each { |i| buffer.write i }
     buffer.read
