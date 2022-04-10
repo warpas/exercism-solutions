7021e6e629866bb893b6c347877421a6a92a5f07
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-04-10 21:39:55 +0200

    Add Ruby track exercise Wordy

---
+++ ruby/wordy/wordy.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Wordy' exercise in this file. Make the tests in
+`wordy_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/wordy` directory.
+=end
