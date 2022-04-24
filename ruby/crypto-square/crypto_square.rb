77b32b1f207b7832a905da02ab0597ee90282695
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-04-24 17:00:21 +0200

    Add Ruby track solution Crypto Square iteration #1.2 - refactoring instance variables out, in progress

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -9,20 +9,22 @@ class Crypto
 
   # @return [String] encoded version of the message
   def ciphertext
-    @normalized = normalized_text
-    return @normalized if @normalized.empty?
+    normalized_message = normalize_text
+    return '' if normalized_message.empty?
 
-    @columns, @rows = square_sides
+    @columns, @rows = square_sides(normalized_message.length)
+    # p @columns
+    # p @rows
     @square = square
 
     built = []
     columns.times do |row|
       line = []
       columns.times do |col|
-        break if square[col].nil?
+        break if @square[col].nil?
 
-        line << square[col][row] unless square[col][row].nil?
-        line << ' ' if square[col][row].nil?
+        line << @square[col][row] unless @square[col][row].nil?
+        line << ' ' if @square[col][row].nil?
       end
       built << line.join
     end
@@ -34,17 +36,24 @@ class Crypto
 
   attr_reader :columns, :rows
 
-  def normalized_text
-    @message.downcase.scan(/\w+/).join
+  def normalize_text
+    # normalized_no_join = @message.downcase.scan(/\w+/)#.join
+    normalized_with_join = @message.downcase.scan(/\w+/).join
+    # n_length_no_join = normalized_no_join.join.length
+    # n_length_with_join = normalized_with_join.length
+    # p "n_length_no_join = #{n_length_no_join}"
+    # p "n_length_with_join = #{n_length_with_join}"
+    @n_length = normalized_with_join.length
+    @normalized = normalized_with_join
   end
 
-  def square_sides
-    return 0 if @normalized.nil?
+  def square_sides(message_length)
+    # return 0 if @normalized.nil?
 
-    square_root = Math.sqrt(@normalized.length)
+    square_root = Math.sqrt(message_length)
     square_root_floor = square_root.floor
     longer_side = square_root.floor
-    longer_side += 1 unless (@normalized.length % square_root).zero?
+    longer_side += 1 unless (message_length % square_root).zero?
     [longer_side, square_root_floor]
   end
 
