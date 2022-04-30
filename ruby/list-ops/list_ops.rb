e89d00ab469bf14edb01085a85246cd70ed1de26
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-04-30 11:32:14 +0200

    Add Ruby track solution for List Ops iteration#0.1 - base structure

--- ruby/list-ops/list_ops.rb
+++ ruby/list-ops/list_ops.rb
@@ -1,7 +1,5 @@
-=begin
-Write your code for the 'List Ops' exercise in this file. Make the tests in
-`list_ops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/list-ops` directory.
-=end
+class ListOps
+  def self.arrays(_)
+    0
+  end
+end
