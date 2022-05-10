7253918ff2c0eea2ca3f84ca37bb24995e2b3e12
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-05-10 19:19:53 +0200

    Add Ruby track solution for Roman Numerals iteration#1

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -2,23 +2,50 @@ class Integer
   def to_roman
     roman_numeral = ''
 
-    ten_plus = self / 10
+    thousand = self / 1000
+    roman_numeral += 'M' * thousand if thousand.positive?
+    less_than_thousand = self % 1000
 
-    roman_numeral += 'X' * ten_plus
+    five_hundred = less_than_thousand / 500
+    less_than_five_hundred = less_than_thousand % 500
+    if less_than_thousand >= 900
+      roman_numeral += 'CM'
+      less_than_five_hundred = less_than_thousand % 900
+    elsif five_hundred.positive?
+      roman_numeral += 'D'
+    end
 
-    last_digit = self % 10
+    hundred = less_than_five_hundred / 100
+    less_than_hundred = less_than_five_hundred % 100
+    if less_than_five_hundred >= 400
+      roman_numeral += 'CD'
+    elsif hundred.positive?
+      roman_numeral += 'C'
+    end
 
-    return 'IX' if last_digit == 9
-    return 'IV' if last_digit == 4
+    fifty = less_than_hundred / 50
+    less_than_fifty = less_than_hundred % 50
+    if less_than_hundred >= 90
+      roman_numeral += 'XC'
+      less_than_fifty = less_than_hundred % 90
+    elsif fifty.positive?
+      roman_numeral += 'L'
+    end
+
+    ten_plus = less_than_fifty / 10
+    if ten_plus >= 4
+      roman_numeral += 'XL'
+    else
+      roman_numeral += 'X' * ten_plus
+    end
+    last_digit = less_than_fifty % 10
+
+    return roman_numeral + 'IX' if last_digit == 9
+    return roman_numeral + 'IV' if last_digit == 4
 
     five = last_digit / 5
-
     roman_numeral += 'V' if five.positive?
-
     last_five = last_digit % 5
-
-    return 'VI' if last_digit == 6
-    return 'V' if last_digit == 5
 
     roman_numeral += 'I' * last_five
     roman_numeral
--- ruby/roman-numerals/roman_numerals_test.rb
+++ ruby/roman-numerals/roman_numerals_test.rb
@@ -36,57 +36,46 @@ class RomanNumeralsTest < Minitest::Test
   end
 
   def test_48_is_not_50_2_but_rather_40_8 # rubocop:disable Naming/VariableNumber
-    skip
     assert_equal 'XLVIII', 48.to_roman
   end
 
   def test_49_is_not_40_5_4_but_rather_50_10_10_1 # rubocop:disable Naming/VariableNumber
-    skip
     assert_equal 'XLIX', 49.to_roman
   end
 
   def test_50_is_a_single_l
-    skip
     assert_equal 'LIX', 59.to_roman
   end
 
   def test_90_being_100_10_is_xc
-    skip
     assert_equal 'XCIII', 93.to_roman
   end
 
   def test_100_is_a_single_c
-    skip
     assert_equal 'CXLI', 141.to_roman
   end
 
   def test_60_being_50_10_is_lx
-    skip
     assert_equal 'CLXIII', 163.to_roman
   end
 
   def test_400_being_500_100_is_cd
-    skip
     assert_equal 'CDII', 402.to_roman
   end
 
   def test_500_is_a_single_d
-    skip
     assert_equal 'DLXXV', 575.to_roman
   end
 
   def test_900_being_1000_100_is_cm
-    skip
     assert_equal 'CMXI', 911.to_roman
   end
 
   def test_1000_is_a_single_m
-    skip
     assert_equal 'MXXIV', 1024.to_roman
   end
 
   def test_3000_is_three_m_s
-    skip
     assert_equal 'MMM', 3000.to_roman
   end
 end
