b507432be259c6c5d5a41223ce31d4c3697a6b15
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-05-11 17:28:16 +0200

    Add Ruby track solution for Roman Numerals iteration#1.1 - attempt to extact a method for converting a single symbol

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -32,22 +32,46 @@ class Integer
       roman_numeral += 'L'
     end
 
-    ten_plus = less_than_fifty / 10
-    if ten_plus >= 4
-      roman_numeral += 'XL'
-    else
-      roman_numeral += 'X' * ten_plus
-    end
-    last_digit = less_than_fifty % 10
+    to_roman, last_digit = roman_part(less_than_fifty, 'X', 10)
+    roman_numeral += to_roman
+
+    # ten_plus = less_than_fifty / 10
+    # if ten_plus >= 4
+    #   roman_numeral += 'XL'
+    # else
+    #   roman_numeral += 'X' * ten_plus
+    # end
+    # last_digit = less_than_fifty % 10
 
     return roman_numeral + 'IX' if last_digit == 9
     return roman_numeral + 'IV' if last_digit == 4
 
-    five = last_digit / 5
-    roman_numeral += 'V' if five.positive?
-    last_five = last_digit % 5
 
+    to_roman, last_five = roman_part(last_digit, 'V', 5)
+    # five = last_digit / 5
+    # roman_numeral += 'V' if five.positive?
+    # last_five = last_digit % 5
+
+    roman_numeral += to_roman
     roman_numeral += 'I' * last_five
     roman_numeral
+  end
+
+  private
+
+  def roman_part(number_to_convert, symbol, limit)
+    roman_addition = ''
+    five = number_to_convert / limit
+    leftover_number = number_to_convert % limit
+    # p five
+    # if leftover_number >= number_to_convert - 1
+    if five >= 4
+      roman_addition += 'XL'
+    elsif five.positive?
+      roman_addition += symbol * five
+    end
+    # roman_addition += symbol if five.positive?
+
+    [roman_addition, leftover_number]
   end
 end
