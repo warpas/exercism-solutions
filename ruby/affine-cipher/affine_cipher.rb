62fa5b97dd95387cba4bbe3b02769e20111bce77
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-05-13 22:12:21 +0200

    Add Ruby track exercise Affine Cipher

---
+++ ruby/affine-cipher/affine_cipher.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Affine Cipher' exercise in this file. Make the tests in
+`affine_cipher_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/affine-cipher` directory.
+=end
