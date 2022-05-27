8777590baaa0fefdbfa2a3c05c8f341ae83a44bc
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-05-27 22:13:52 +0200

    Add Ruby track exercise Rotational Cipher

---
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Rotational Cipher' exercise in this file. Make the tests in
+`rotational_cipher_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/rotational-cipher` directory.
+=end
