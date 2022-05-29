53ece6a465d75cf4e7b8859bc6a6539529c17650
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-05-29 10:55:26 +0200

    Add Ruby track solution for Kindergarten Garden iteration#0.3 - calculated for all 3 kids

--- ruby/kindergarten-garden/kindergarten_garden.rb
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -1,21 +1,26 @@
 class Garden
   def initialize(arg)
-    p arg
-    @garden_string = arg
+    # p arg
+    garden_string = arg
+    line1, line2 = garden_string.lines
+    # p "line1 = #{line1}, line2 = #{line2}"
+    translated_line1 = line1.chars.map { |elem| DICTIONARY[elem] }.reject(&:nil?)
+    translated_line2 = line2.chars.map { |elem| DICTIONARY[elem] }.reject(&:nil?)
+    # p "t_line1 = #{translated_line1}, t_line2 = #{translated_line2}"
+    @translated = translated_line1.zip(translated_line2)
+    # p "end of input"
   end
 
   def alice
-    # [:radishes, :clover, :grass, :grass]
-    p(@garden_string.chars.map { |elem| DICTIONARY[elem] }.reject(&:nil?))
+    build_garden(:alice)
   end
 
   def bob
-    # [:clover, :grass, :radishes, :clover]
-    [:clover, :clover, :clover, :clover]
+    build_garden(:bob)
   end
 
   def charlie
-    [:grass, :grass, :grass, :grass]
+    build_garden(:charlie)
   end
 
   private
@@ -24,8 +29,24 @@ class Garden
     'R' => :radishes,
     'C' => :clover,
     'G' => :grass,
-    'V' => :violets,
-  }
+    'V' => :violets
+  }.freeze
+
+  ORDER = {
+    alice: [0, 1],
+    bob: [2, 3],
+    charlie: [4, 5]
+  }.freeze
 
   private_constant :DICTIONARY
+
+  def build_garden(owner)
+    the_garden_front = []
+    the_garden_back = []
+    ORDER[owner].each do |index|
+      the_garden_front << @translated[index].first
+      the_garden_back << @translated[index].last
+    end
+    the_garden_front + the_garden_back
+  end
 end
