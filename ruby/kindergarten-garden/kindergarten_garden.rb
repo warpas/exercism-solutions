4736b82e107977cf04891a2ade6d2a147c1e59a2
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-06-09 08:22:50 +0200

    Add Ruby track solution for Kindergarten Garden iteration#1.4 - clean up the initialize method

--- ruby/kindergarten-garden/kindergarten_garden.rb
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -1,14 +1,12 @@
 class Garden
-  def initialize(arg, students = DEFAULT_ORDER)
-    garden_string = arg
-    line1, line2 = garden_string.lines
-    @students = students
-    @student_order = save_order(students)
-    @students.each do |name|
+  def initialize(garden_string, students = DEFAULT_ORDER)
+    students.each do |name|
       define_singleton_method(name.downcase) do
         build_garden(name.downcase.to_sym)
       end
     end
+    @student_order = save_order(students)
+    line1, line2 = garden_string.lines
     translated_line1 = line1.chars.map { |elem| DICTIONARY[elem] }.reject(&:nil?)
     translated_line2 = line2.chars.map { |elem| DICTIONARY[elem] }.reject(&:nil?)
     @translated = translated_line1.zip(translated_line2)
