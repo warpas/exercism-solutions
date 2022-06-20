239158b9e8ce53cd30b7e3c249b3934bf4719397
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-06-20 08:24:26 +0200

    Add Ruby track solution for Affine Cipher iteration#0.2 - add encode logic for continuous strings of small letters

--- ruby/affine-cipher/affine_cipher.rb
+++ ruby/affine-cipher/affine_cipher.rb
@@ -2,10 +2,34 @@
 
 # Implementation of the Affine Cipher exercise in Ruby track on Exercism.
 class Affine
-  def initialize(_, _)
+  def initialize(a, b)
+    puts ""
+    p @a = a
+    p @b = b
   end
 
-  def encode(_)
-    'xbt'
+  def encode(text)
+    a_byte = 0
+    'a'.each_byte do |a|
+      a_byte = a
+    end
+    p a_byte
+    p text
+    bytes = []
+    text.each_byte do |c|
+      puts "inside the encode loop"
+      p c
+      byte_diff = c - a_byte
+      p byte_diff
+      calculated_encoding = (@a * byte_diff + @b) % ALPHABET_LENGTH
+      bytes << calculated_encoding + a_byte
+      p calculated_encoding
+    end
+    p bytes
+    bytes.pack('c*')
   end
+
+  ALPHABET_LENGTH = 26
+
+  private_constant :ALPHABET_LENGTH
 end
