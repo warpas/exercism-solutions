7398060de223136697629f93c6987a3458a1bbb4
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-07-03 11:20:49 +0200

    Add JavaScript track solution for Annalyn's Infiltration iteration#0.1 - pass first 2 tests

--- javascript/annalyns-infiltration/annalyns-infiltration.js
+++ javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -27,7 +27,7 @@
  * @return {boolean} Whether or not you can execute a fast attack.
  */
 export function canExecuteFastAttack(knightIsAwake) {
-  throw new Error('Remove this line and implement the function');
+  return !knightIsAwake
 }
 
 /**
