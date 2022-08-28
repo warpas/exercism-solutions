0b907ed050a8493531967e09b33794e93d8cfdca
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-08-28 13:39:33 +0200

    Add Rust track solution for Assembly Line iteration#0.4 - add the simplest solution to the 5th test - add else if to the previous condition

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -5,8 +5,10 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     let success_rate: f64 = if speed < 5 {
         1.0
-    } else {
+    } else if speed < 9 {
         0.9
+    } else {
+        0.77
     };
     let speed_float: f64 = <u8 as Into<f64>>::into(speed);
     let production_rate: f64 = 221.0;
