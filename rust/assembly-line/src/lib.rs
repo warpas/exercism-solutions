5329e36be8cff764d770a6a0b02199d233f45b6b
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-08-31 17:47:33 +0200

    Add Rust track solution for Assembly Line iteration#1.1 - simplify the type conversions

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -10,13 +10,12 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
     } else {
         0.77
     };
-    let speed_float: f64 = speed.into();
+    let speed_float = speed as f64;
     let production_rate: f64 = 221.0;
     speed_float * production_rate * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    let per_minute = production_rate_per_hour(speed) / 60.0;
-    let result:u32 = per_minute as u32;
-    result
+    let per_minute = (production_rate_per_hour(speed) / 60.0) as u32;
+    per_minute
 }
