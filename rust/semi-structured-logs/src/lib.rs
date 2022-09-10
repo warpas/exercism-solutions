a508902162e18dd23b883582ba0e332762f938f5
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-09-10 14:12:15 +0200

    Add Rust track solution for Semi Structured Logs iteration#0.9 - add the Debug log level to enable the feature gated test - remove a bunch of obsolete comments

--- rust/semi-structured-logs/src/lib.rs
+++ rust/semi-structured-logs/src/lib.rs
@@ -2,40 +2,21 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
-// use log::Level;
-// use log::info;
-
 /// various log levels
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug,
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    // let levelsLevels = level.to_string();
-    // format!("[{}]: Timezone changed", levelsLevels)
-
     println!("!!!!!!!!! start");
-    // println()
-    // if level ==
     println!("{}", message);
 
-    // let success_rate: f64 = if speed < 5 {
-    //     1.0
-    // } else if speed < 9 {
-    //     0.9
-    // } else {
-    //     0.77
-    // };
     if level == LogLevel::Warning {
         println!("LogLevel::Warning");
-        // let computed: String = "[WARNING]: ".to_owned() + message;
-        // let computed: String = format!("[WARNING]: {}", message);
-        // return computed
-        // return format!("[WARNING]: {}", message)
-        // return "[WARNING]: ".to_owned() + message
         return warn(message)
     }
     if level == LogLevel::Error {
@@ -46,20 +27,11 @@ pub fn log(level: LogLevel, message: &str) -> String {
         println!("LogLevel::Info");
         return info(message)
     }
-    // println!("{}", level);
-    // `LogLevel` cannot be formatted with the default formatter. What do?
 
-    // TODO: use the level variable
-    // TODO: use the message variable
     println!("!!!!!!!!! end");
     return String::from("[INFO]: Timezone changed");
 }
-// fn main() {
-//     let a = "AAA";
-//     let b = format!("BBB {}", a);
-//     println(b);
-// }
-// output: BBB AAA
+
 pub fn info(message: &str) -> String {
     format!("[INFO]: {}", message)
 }
