df82b9fa608bd9009e162ea9b349ba743d1db134
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-09-22 08:26:32 +0200

    Add Ruby track solution for Affine Cipher iteration#2 - remove puts and debug lines

--- ruby/affine-cipher/affine_cipher.rb
+++ ruby/affine-cipher/affine_cipher.rb
@@ -7,51 +7,35 @@ class Affine
 
     @key_component_a = key_component_a
     @key_component_b = key_component_b
-    # p "@key_a = #{@key_component_a}, @key_b = #{@key_component_b}, start_of_alphabet = #{start_of_alphabet}"
-    # encode('exercism')
   end
 
   def encode(text)
-    # puts "ENCODE"
-    # p text
     prepared_text = text.downcase.split(/[-\s]/).join
     without_commas = prepared_text.split(/,/).join
     processed_text = without_commas.split(/[.]/).join
-    # p processed_text
+
     bytes = []
     processed_text.each_byte do |c|
       bytes << encoded_character(c)
     end
-    # p bytes
     result = bytes.pack('c*')
-
-    # p insert_spaces(result)
     insert_spaces(result)
   end
 
-  ALPHABET_LENGTH = 26
-
   def decode(text)
-    # puts "DECODE"
-    # p text
-
     @mapping = calculate_mapping
-
     processed_text = text.downcase.split(/[-\s]/).join
-    # 'exercism'
 
     bytes = []
     processed_text.each_byte do |c|
       bytes << decoded_character(c)
     end
-
-    result = bytes.pack('c*')
-
-    result
+    bytes.pack('c*')
   end
 
   private
 
+  ALPHABET_LENGTH = 26
   private_constant :ALPHABET_LENGTH
 
   def start_of_alphabet
@@ -68,8 +52,6 @@ class Affine
 
     byte_diff = char - start_of_alphabet
     calculated_encoding = (@key_component_a * byte_diff + @key_component_b) % ALPHABET_LENGTH
-
-    # p "#{char} -> #{calculated_encoding + start_of_alphabet}, byte_diff = #{byte_diff}, calc_enc = #{(@key_component_a * byte_diff + @key_component_b)}, calc_enc MOD = #{calculated_encoding}"
     calculated_encoding + start_of_alphabet
   end
 
@@ -95,31 +77,15 @@ class Affine
     return char if char <= 60 && char > 47
 
     byte_diff = char - start_of_alphabet
-
-    # calculated_encoding_no_mod = (byte_diff * @key_component_a + @key_component_b)
-    # calculated_encoding = calculated_encoding_no_mod % ALPHABET_LENGTH
-    # 116 -> 101
-
-    # 4 * 3 + 7
-    # 12 + 7
-    # 19
-
-    # 19 / 3 = 3
-
-    # p "#{char} -> #{calculated_encoding + start_of_alphabet}, byte_diff = #{byte_diff}, calc_enc = #{calculated_encoding_no_mod}, calc_enc MOD = #{calculated_encoding}"
-    # p "#{byte_diff} -> #{@mapping[byte_diff]}"
     @mapping[byte_diff] + start_of_alphabet
-    # char
   end
 
   def calculate_mapping
     mapping = {}
 
     (0..25).each do |key|
-      # p key
       formula = @key_component_a * key + @key_component_b
       encoding = formula % ALPHABET_LENGTH
-      # p "#{key} #{encoding}"
       mapping[encoding] = key
     end
 
