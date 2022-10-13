6d15c618a66f66911c92c8b1ec942db81ea1360f
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-10-13 16:24:13 +0200

    Add Rust track solution for Resistor Color iteration#0.2.1 - clean up prints from the solution

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -14,48 +14,16 @@ pub enum ResistorColor {
 }
 
 pub fn color_to_value(color: ResistorColor) -> u32 {
-    // 0
-    let mut considered_index = 0;
-    let first_color;
-    // match get_an_optional_value() {
-    //     Some(v) => x = v, // if Some("abc"), set x to "abc"
-    //     None => panic!(), // if None, panic without any message
-    // }
-    println!("{:?}", color);
-    match get_first() {
-        Some(x) => first_color = x,
-        None => panic!()
-    }
-    let mut all_colors = get_all();
-
-    println!("{:?}", all_colors);
-    // println!("{:?}", all_colors.next());
-    // println!("{:?}", all_colors.next());
-    // println!("{:?}", all_colors.next());
-
-    let mut considered_color = all_colors.next();
-    let mut are_we_there_yet: bool = false;
     let wrapped_color: Option<ResistorColor> = Some(color);
-    println!("------ Loop starting");
-    while !are_we_there_yet {
-        are_we_there_yet = considered_color == wrapped_color;
-        println!("{:?}", all_colors);
-        println!("{:?}, index {:?}", considered_color, considered_index);
-        println!("considered: {:?}, target: {:?}", considered_color, wrapped_color);
-        println!("{:?}", are_we_there_yet);
-        considered_color = all_colors.next();
+    let mut all_colors = get_all();
+    let mut considered_color = all_colors.next();
+    let mut considered_index = 0;
+    while considered_color != wrapped_color {
         considered_index = considered_index + 1;
+        considered_color = all_colors.next();
     }
 
-    println!("{:?}", first_color);
-    println!("{:?}", wrapped_color);
-    println!("considered: {:?}, target: {:?}", considered_color, wrapped_color);
-    println!("{:?}", are_we_there_yet);
-
-    let result: u32 = considered_index - 1;
-    println!("{:?}", result);
-
-    result
+    considered_index
 }
 
 pub fn value_to_color_string(value: u32) -> String {
@@ -67,10 +35,6 @@ pub fn value_to_color_string(value: u32) -> String {
 
 pub fn colors() -> Vec<ResistorColor> {
     unimplemented!("return a list of all the colors ordered by resistance")
-}
-
-fn get_first() -> Option<ResistorColor> {
-    first::<ResistorColor>()
 }
 
 fn get_all() -> All<ResistorColor> {
