c5b81e2657e80ddeac6f0f165e0c1de980a9c4e5
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-10-17 08:13:12 +0200

    Add Rust track solution for Health Statistics iteration#0.1 - add the implementation of the 'new' function

--- rust/health-statistics/src/lib.rs
+++ rust/health-statistics/src/lib.rs
@@ -10,7 +10,7 @@ pub struct User {
 
 impl User {
     pub fn new(name: String, age: u32, weight: f32) -> Self {
-        unimplemented!()
+        { User { name: name, age: age, weight: weight } }
     }
 
     pub fn name(&self) -> &str {
