3c954b8b6619f5f5d3d2844162c57ac2e0d0bbba
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-11-10 17:10:55 +0100

    Add Rust track solution for A Short Fibonacci Sequence iteration#0.2.3 - recreate the vector with the vec! macro - successfully push a value to mutate the vector

--- rust/short-fibonacci/src/lib.rs
+++ rust/short-fibonacci/src/lib.rs
@@ -8,8 +8,12 @@ pub fn create_empty() -> Vec<u8> {
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
     // unimplemented!("create a zeroized buffer of {} bytes", count)
-    let my_vector = Vec::new();
-    my_vector.push(1)
+    // let my_vector = Vec::new();
+    let mut my_vector = vec![1u8, 2, 3];
+    println!("Initial vector: {:?}", my_vector);
+    my_vector.push(1);
+    println!("Updated vector: {:?}", my_vector);
+    my_vector
 }
 
 /// Create a vector containing the first five elements of the Fibonacci sequence.
