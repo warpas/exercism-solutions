24d3cad8681b892e0b2506902a1737e1db98e9f8
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-11-19 12:09:25 +0100

    Add Rust track solution for Gigasecond iteration#0.1 - import Date and Time - attempt to create a DateTime struct

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,13 @@
 use time::PrimitiveDateTime as DateTime;
+use time::Date;
+use time::Time;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    // unimplemented!("What time is a gigasecond later than {}", start);
+    // DateTime::new(Date::new(), 0)
+    DateTime::new(
+        Date::from_calendar_date(2022, 11, 19).unwrap(),
+        Time::from_hms(12, 8, 10).unwrap(),
+    )
 }
