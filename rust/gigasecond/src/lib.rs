9b239c7170bbb138c072b39439c065663dd6cebe
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-11-21 08:10:37 +0100

    Add Rust track solution for Gigasecond iteration#0.1.1 - add usage of the time::Month enum - change the date to the one in the test

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -7,7 +7,7 @@ pub fn after(start: DateTime) -> DateTime {
     // unimplemented!("What time is a gigasecond later than {}", start);
     // DateTime::new(Date::new(), 0)
     DateTime::new(
-        Date::from_calendar_date(2022, 11, 19).unwrap(),
-        Time::from_hms(12, 8, 10).unwrap(),
+        Date::from_calendar_date(2043, time::Month::January, 1).unwrap(),
+        Time::from_hms(1, 46, 40).unwrap(),
     )
 }
