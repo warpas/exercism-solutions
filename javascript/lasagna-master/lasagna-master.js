d00c645dca586f0c51fe32a69f87946c47afd67a
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2022-12-04 10:07:13 +0100

    Add JavaScript track solution for Lasagna Master iteration#0.5.1 - read the issue description - add the simplest correct implementation of 'addSecretIngredient' - pass the fourth set of tests 🎉

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -45,6 +45,7 @@ export function quantities(layersArray) {
   return ingredients;
 }
 
-export function addSecretIngredient(firendList, myList) {
-  return firendList.concat(myList);
+export function addSecretIngredient(friendList, myList) {
+  const secretIngredient = friendList[friendList.length-1]
+  myList[myList.length] = secretIngredient
 }
