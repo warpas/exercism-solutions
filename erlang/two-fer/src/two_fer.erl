7e74b01f892e13979f2feab4ec5bb90371942fca
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-02-17 21:44:24 +0100

    Add Erlang track solution for Two Fer iteration#1.0 - implement the `two_fer` function/predicate - use `[strings, to, concatenate]` because `concat/2` is obsolete - pass all tests - submit the solution

--- erlang/two-fer/src/two_fer.erl
+++ erlang/two-fer/src/two_fer.erl
@@ -3,6 +3,6 @@
 -export([two_fer/0, two_fer/1]).
 
 
-two_fer() -> undefined.
+two_fer() -> "One for you, one for me.".
 
-two_fer(_Name) -> undefined.
+two_fer(Name) -> ["One for ", Name, ", one for me."].
