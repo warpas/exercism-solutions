f35748f8369a0c91f9bd2ff762fe07b8c1b9e6e8
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-02-17 22:33:22 +0100

    Add Haskell track solution for Space Age iteration#1.0 - implement the `ageOn` function - define a helper `ageOnEarth` function - add a naive set of implementations for all cases - pass all the tests - submit the solution

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,4 +10,14 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn Earth seconds = ageOnEarth seconds
+ageOn Mercury seconds = (ageOnEarth seconds) / 0.2408467
+ageOn Venus seconds = (ageOnEarth seconds) / 0.61519726
+ageOn Mars seconds = (ageOnEarth seconds) / 1.8808158
+ageOn Jupiter seconds = (ageOnEarth seconds) / 11.862615
+ageOn Saturn seconds = (ageOnEarth seconds) / 29.447498
+ageOn Uranus seconds = (ageOnEarth seconds) / 84.016846
+ageOn Neptune seconds = (ageOnEarth seconds) / 164.79132
+
+ageOnEarth :: Float -> Float
+ageOnEarth seconds = seconds / 365.25 / 24 / 3600
