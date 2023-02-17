8ee6182a12bea1df989341c8b221807c247ac54b
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-02-17 22:07:30 +0100

    Add Erlang track solution for Leap iteration#1.0 - implement the `leap_year` function - use Erlang guard clauses for the first time - use modulo operator, written as `rem` - not equal zero was written as `not(Year rem 100 == 0)` because `!=` does not work - pass all the tests - submit the solution

--- erlang/leap/src/leap.erl
+++ erlang/leap/src/leap.erl
@@ -3,4 +3,6 @@
 -export([leap_year/1]).
 
 
-leap_year(_Year) -> undefined.
+leap_year(Year) when Year rem 400 == 0 -> true;
+leap_year(Year) when (Year rem 4 == 0) and not(Year rem 100 == 0) -> true;
+leap_year(_Year) -> false.
