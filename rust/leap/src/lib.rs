c93644048269fe3432c2eb32b9e7f9c3e77c1873
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-03-05 17:24:41 +0200

    Add Rust track solution for Leap iteration#2.0 - add the is_divisible_by function - signature: `fn is_divisible_by(year: u64, divisor: u64) -> bool` - rewrite the `is_leap_year` function to be more readable with `is_divisible_by` - submit the 2nd iteration of the solution

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
-    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+    is_divisible_by(year, 4)
+        && !is_divisible_by(year, 100)
+        || is_divisible_by(year, 400)
+}
+
+fn is_divisible_by(year: u64, divisor: u64) -> bool {
+    year % divisor == 0
 }
