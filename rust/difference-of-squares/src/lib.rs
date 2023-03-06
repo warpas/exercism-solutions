5ea8635fe12973cede640ae2ceb6911a6a6eede5
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-03-06 18:23:15 +0200

    Add Rust track solution for Difference of Squares iteration#2.0 - build failed on Exercism - revert to the previous version (before extracting `vector_from_1_to`) - submit the 2nd iteration of the solution

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,23 +1,19 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let sum: u32 = vector_from_1_to(n)
-        .iter()
-        .sum();
-    sum * sum
-}
-
-pub fn sum_of_squares(n: u32) -> u32 {
-    vector_from_1_to(n)
-        .iter()
-        .map(|v| v * v)
-        .sum()
-}
-
-fn vector_from_1_to(n: u32) -> Vec<u32> {
     let mut from_1_to_n: Vec<u32> = vec![];
     for i in 0..n {
         from_1_to_n.push(i + 1)
     }
-    from_1_to_n
+    let sum: u32 = from_1_to_n.iter().sum();
+    sum * sum
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    let mut from_1_to_n: Vec<u32> = vec![];
+    for i in 0..n {
+        from_1_to_n.push(i + 1)
+    }
+    let sum: u32 = from_1_to_n.iter().map(|v| v * v).sum();
+    sum
 }
 
 pub fn difference(n: u32) -> u32 {
