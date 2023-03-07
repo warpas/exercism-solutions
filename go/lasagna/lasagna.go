4d775993105e4303faa301df289c73b040766200
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-03-07 15:20:22 +0200

    Add Go track solution for Gopher's Gorgeous Lasagna iteration#1.0 - remind myself that to run tests I need to `go test` - guessed the constant definition syntax (`const OvenTime = 40`) - easy exercise, pass all tests - submit the solution

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -1,18 +1,19 @@
 package lasagna
 
-// TODO: define the 'OvenTime' constant
+const OvenTime = 40
+const PrepTimePerLayer = 2
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
 func RemainingOvenTime(actualMinutesInOven int) int {
-	panic("RemainingOvenTime not implemented")
+	return OvenTime - actualMinutesInOven
 }
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
 func PreparationTime(numberOfLayers int) int {
-	panic("PreparationTime not implemented")
+	return PrepTimePerLayer * numberOfLayers
 }
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
-	panic("ElapsedTime not implemented")
+	return actualMinutesInOven + PreparationTime(numberOfLayers)
 }
