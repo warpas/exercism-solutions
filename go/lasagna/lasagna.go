9872c542d8e05b450add3a4895ce1380e8e6663d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-03-07 15:18:17 +0200

    Add Go track solution for Gopher's Gorgeous Lasagna iteration#0.0 - add the exercise

---
+++ go/lasagna/lasagna.go
@@ -0,0 +1,18 @@
+package lasagna
+
+// TODO: define the 'OvenTime' constant
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	panic("RemainingOvenTime not implemented")
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	panic("PreparationTime not implemented")
+}
+
+// ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	panic("ElapsedTime not implemented")
+}
