606cf6d81a77d883558e33eebabc3d14a112d6ca
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-03-07 15:44:53 +0200

    Add Go track solution for Weather Forecast iteration#1.0 - add comments   - Surprised that they all need to be formatted in a specific way. - pass all tests - submit the solution

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather is a limited implementation formatting weather forcasts.
 package weather
 
+// CurrentCondition variable represents the current condition as a string.
 var CurrentCondition string
+
+// CurrentLocation variable represents the current location as a string.
 var CurrentLocation string
 
+// Forecast function takes a city and condition and formats them as a string on the output.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
