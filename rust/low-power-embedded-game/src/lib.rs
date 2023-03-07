b21766ce5657f901e032941295057fdb5dd8ac05
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-03-07 22:05:47 +0200

    Add Rust track solution for Low-Power Embedded Game iteration#0.9 - I give up. This is infuriating!   - I don't know how to print out partial results to proceed in small steps   - Iterators are not obvious to work with. I need to read and experiment a lot   - generic types on top of Iterators is way too much for me.   - all of this combined makes me want to throw my computer out the window. - I read up on Debug in Rust Book and Rust by Example - added a new simpler version of 'evens' function without generic types to debug

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -1,6 +1,7 @@
 // This stub file contains items that aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
 #![allow(unused)]
+// #[derive(Debug)]
 
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
     let result = dividend / divisor;
@@ -8,6 +9,14 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
     (result, remainder)
 }
 
+pub fn simple_evens(iter: impl Iterator) -> impl Iterator {
+// pub fn evens<usize>(iter: impl Iterator<usize>) -> impl Iterator<usize> {
+    // println!("{:#?}", iter)
+    // println!("{:#?}", iter.enumerate())
+    iter
+}
+
+// pub fn evens<usize>(iter: impl Iterator<Item = usize> + std::fmt::Debug) -> impl Iterator<Item = usize> {
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
 
     // println!("{:}", iter);
@@ -28,7 +37,17 @@ pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
     // unimplemented!("implement `fn evens`");
     // // TODO: remove this; it's only necessary to allow this function to compile
     // // before the student has done any work.
-    std::iter::empty()
+    // std::iter::empty()
+    // (0..)
+    // iter.map(|n| n * n)
+    // Enumerate::<i, val> = iter.enumerate();
+    // println!("{:?}", iter.enumerate());
+
+    // let new_iter = iter.enumerate();
+    // println!("{:?}", iter.inspect());
+    // let new_iter = iter.enumerate().filter(|(i, n)| i % 2 == 0);
+    // println!("{:?}", new_iter.inspect());
+    iter
 
 
     // let v_test1: Vec<T> = vec![0, 2, 4, 6, 8];
