faa0854c6de755e3b760be887fd72590a5c9ed2b
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-03-24 19:19:26 +0100

    Add Rust track solution for Gigasecond iteration#0.4 - retry the exercise using the `checked_add` function from the `time` crate - update the `time` crate dependency to use `macros` - use the `datetime!` macro as a date constructor that I can understand - try to use the `Duration` struct (from `time` crate) as the type for `giga` - remind myself how to use `println!` macro for debugging purposes - finally manage to pass a valid argument to `checked_add`! - add a `match` to unwrap the output of `checked_add` - pass the tests!

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,15 +1,31 @@
 use time::PrimitiveDateTime as DateTime;
 use time::Date;
 use time::Time;
+use time::macros::{date, datetime, offset};
+use time::Duration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    let giga = 1_000_000_000;
+    let giga: Duration = Duration::seconds(1_000_000_000);
     // let after = start + giga;
-    println!("{:?}, giga {:?}", start, giga);
+    println!("start {:?}, giga {:?}", start, giga);
     // println!("{:?}", after);
-    DateTime::new(
-        Date::from_calendar_date(2043, time::Month::January, 1).unwrap(),
-        Time::from_hms(1, 46, 40).unwrap(),
-    )
+    // DateTime::new(
+    //     Date::from_calendar_date(2043, time::Month::January, 1).unwrap(),
+    //     Time::from_hms(1, 46, 40).unwrap(),
+    // )
+    // PrimitiveDateTime::new(date!(2043-01-01), time!(0:00))
+    // datetime!(2019-01-01 0:00)
+    let date_something = start.checked_add(giga);
+    println!("{:?}", date_something);
+    let date_output =
+        match start.checked_add(giga) {
+            Some(x) => x,
+            None => todo!(),
+        };
+    date_output
+
+    // datetime!(2043-01-01 1:46:40)
+    // use checked_add function
+
 }
