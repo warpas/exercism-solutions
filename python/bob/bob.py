b6e923e0492cc60713e4270d1850c8cd9270de7c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-04-28 22:30:43 +0200

    Add Python track solution for Bob iteration#1.0 - add the solution translating my Ruby and Elixir solutions - wow, I really like this exercise! - TIL upper case can be done with `str.upper(string_value)` - TIL lower case can be done with `str.lower(string_value)` - TIL stripping whitespace is just `string_value.stip()` - TIL last character check is just `string_value.endswith("?")` - TIL strings are implicitly converted to boolean in Python - TIL `return not string_value.strip()` works because empty string if falsy - I'm still forgetting about explicit returns a lot! - submit the solution

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,20 @@
 def response(hey_bob):
-    pass
+    if(shouting(hey_bob) and question(hey_bob)):
+        return "Calm down, I know what I'm doing!"
+    elif(silence(hey_bob)):
+        return "Fine. Be that way!"
+    elif(shouting(hey_bob)):
+        return "Whoa, chill out!"
+    elif(question(hey_bob)):
+        return "Sure."
+
+    return "Whatever."
+
+def shouting(remark):
+    return remark == str.upper(remark) and remark != str.lower(remark)
+
+def question(remark):
+    return remark.strip().endswith("?")
+
+def silence(remark):
+    return not remark.strip()
