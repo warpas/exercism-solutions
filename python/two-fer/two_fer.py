7b3701bc9aaebe1d021af0156e329b8584b07ec8
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-04-28 21:19:18 +0200

    Add Python track solution for Two Fer iteration#1.1 - TIL Exercism requires docstrings for Python track - add a docstring

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,8 @@
+"""Function for implementing the solution to Exercism's Two Fer exercise on the Python track."""
 def two_fer(name="you"):
+    """Basic string interpolation with a single variable.
+
+    :param name: string - one for who?
+    :return: string - one for each.
+    """
     return "One for " + name + ", one for me."
