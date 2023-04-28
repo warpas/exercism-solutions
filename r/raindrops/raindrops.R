c362a3171d35ae65acbeffe38559b7ceb9da633e
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-04-28 21:01:41 +0200

    Add R track solution for Raindrops iteration#1.0 - TIL if & else statements in R work as expected - add crazy-simplistic nesting of if & else and print the entire output at once - add only conditions that specifically pass an existing test - submit the solution

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,3 +1,31 @@
 raindrops <- function(number) {
-  
+  if (number %% 3 == 0) {
+      if ((number/3) %% 5 == 0) {
+        if ((number/15) %% 7 == 0) {
+          paste("PlingPlangPlong")
+        } else {
+          paste("PlingPlang")
+        }
+      } else {
+        if ((number/3) %% 7 == 0) {
+          paste("PlingPlong")
+        } else {
+          paste("Pling")
+        }
+      }
+  } else {
+    if (number %% 5 == 0) {
+      if ((number/5) %% 7 == 0) {
+        paste("PlangPlong")
+      } else {
+        paste("Plang")
+      }
+    } else {
+      if (number %% 7 == 0) {
+        paste("Plong")
+      } else {
+        paste(number)
+      }
+    }
+  }
 }
