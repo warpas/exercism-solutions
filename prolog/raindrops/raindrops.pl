a24ac2b4e76b951fc431cf79b1a07ad6ae0c81a8
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-05-06 14:14:32 +0200

    Add Prolog track solution for Raindrops iteration#1.0 - add a naive implementation - TIL that I can convert numbers to string with `number_string/2` - submit the solution

--- prolog/raindrops/raindrops.pl
+++ prolog/raindrops/raindrops.pl
@@ -1,1 +1,30 @@
-convert(N, Sounds).
+convert(N, "PlingPlangPlong") :-
+  Nmod105 is N mod 105,
+  Nmod105 = 0,
+  !.
+convert(N, "PlangPlong") :-
+  Nmod35 is N mod 35,
+  Nmod35 = 0,
+  !.
+convert(N, "PlingPlong") :-
+  Nmod21 is N mod 21,
+  Nmod21 = 0,
+  !.
+convert(N, "PlingPlang") :-
+  Nmod15 is N mod 15,
+  Nmod15 = 0,
+  !.
+convert(N, "Plong") :-
+  Nmod7 is N mod 7,
+  Nmod7 = 0,
+  !.
+convert(N, "Plang") :-
+  Nmod5 is N mod 5,
+  Nmod5 = 0,
+  !.
+convert(N, "Pling") :-
+  Nmod3 is N mod 3,
+  Nmod3 = 0,
+  !.
+convert(Number, NumberAsString) :-
+  number_string(Number, NumberAsString).
