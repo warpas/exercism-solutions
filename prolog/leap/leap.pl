cd9e31d20eee2473a535a881433e801dc32a3f87
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-05-06 13:58:39 +0200

    Add Prolog track solution for Leap iteration#1.0 - add a naive implementation - TIL (TIRemembered?) that "not equal" is `\=` - submit the solution

--- prolog/leap/leap.pl
+++ prolog/leap/leap.pl
@@ -1,1 +1,11 @@
-leap(0).
+leap(Year) :-
+  DivisibleBy4 is Year mod 4,
+  DivisibleBy4 = 0,
+  DivisibleBy100 is Year mod 100,
+  DivisibleBy100 \= 0,
+  !.
+
+leap(Year) :-
+  DivisibleBy400 is Year mod 400,
+  DivisibleBy400 = 0,
+  !.
