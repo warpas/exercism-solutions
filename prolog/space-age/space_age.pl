252596a90b518554ddae6baf7ab79814c473f87b
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-05-31 20:43:04 +0200

    Add Prolog track solution for Space Age iteration#1.0 - add a naive implementation - submit the solution

--- prolog/space-age/space_age.pl
+++ prolog/space-age/space_age.pl
@@ -1,1 +1,16 @@
-space_age(Planet, AgeSec, Years).
+space_age("Earth", AgeSec, Years) :-
+  Years is AgeSec / 31_557_600.
+space_age("Mercury", AgeSec, Years) :-
+  Years is AgeSec / (31_557_600 * 0.2408467).
+space_age("Venus", AgeSec, Years) :-
+  Years is AgeSec / (31_557_600 * 0.61519726).
+space_age("Mars", AgeSec, Years) :-
+  Years is AgeSec / (31_557_600 * 1.8808158).
+space_age("Jupiter", AgeSec, Years) :-
+  Years is AgeSec / (31_557_600 * 11.862615).
+space_age("Saturn", AgeSec, Years) :-
+  Years is AgeSec / (31_557_600 * 29.447498).
+space_age("Uranus", AgeSec, Years) :-
+  Years is AgeSec / (31_557_600 * 84.016846).
+space_age("Neptune", AgeSec, Years) :-
+  Years is AgeSec / (31_557_600 * 164.79132).
