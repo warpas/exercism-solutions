a876c93ba8d72f1eaa1986519432ddab8ddb33ae
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-06-30 19:23:00 +0200

    Add Clojure track solution for Lucian's Luscious Lasagna iteration#0.0 - add simple functions based on syntax from [this video](https://www.youtube.com/watch?v=C-kF25fWTO8) - submit the solution

--- clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
+++ clojure/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
@@ -8,6 +8,7 @@
 
 (def expected-time
   ;; Define the var here
+  40
   )
 
 (defn remaining-time
@@ -15,16 +16,19 @@
    and returns how many minutes the lasagna still has to remain in the oven."
   [actual-time]
   ;; Define the function body here
+  (- expected-time actual-time)
   )
 
 (defn prep-time
   "Takes the number of layers added to the lasagna,
    and returns how many minutes you spent preparing the lasagna"
   [num-layers]
+  (* num-layers 2)
   )
 
 (defn total-time
   "Takes the number of layers of lasagna and the actual time in minutes it has been in the oven.
    Returns how many minutes in total you've worked on cooking the lasagna"
   [num-layers actual-time]
+  (+ (prep-time num-layers) actual-time)
   )
