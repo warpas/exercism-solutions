3c30fe3de310b30a674543cd5c3831738884b2b1
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-06-30 20:20:35 +0200

    Add Clojure track solution for Two Fer iteration#0.0 - add the exercise - submit the solution

--- clojure/two-fer/src/two_fer.clj
+++ clojure/two-fer/src/two_fer.clj
@@ -1,5 +1,8 @@
 (ns two-fer)
 
-(defn two-fer [name] ;; <- arglist goes here
-  ;; your code goes here
+(defn two-fer
+  ([] (two-fer "you"))
+  ([name]
+    (str "One for " name ", one for me.")
+  )
 )
