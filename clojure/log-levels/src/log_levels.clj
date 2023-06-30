2dc2fa58bb214d9b2353099ed2bc2d21b1d28476
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-06-30 20:02:55 +0200

    Add Clojure track solution for Log Levels iteration#0.3 - implement log-level as quickly as I can

--- clojure/log-levels/src/log_levels.clj
+++ clojure/log-levels/src/log_levels.clj
@@ -8,13 +8,20 @@
 
   (->> (str/split s #"\[\w+\]:\s*")
        (last)
-       (clojure.string/trimr))
+       (clojure.string/trimr)
+       )
   )
 
 (defn log-level
   "Takes a string representing a log line
    and returns its level in lower-case."
   [s]
+
+  (->> (re-seq #"\[(\w+)\]" s)
+       (last)
+       (map clojure.string/lower-case)
+       (last)
+       )
   )
 
 (defn reformat
