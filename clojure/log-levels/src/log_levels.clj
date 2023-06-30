ff83cc257d92134e6743c723efd61d55f80c05e4
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-06-30 19:55:54 +0200

    Add Clojure track solution for Log Levels iteration#0.2 - improve readability with pipes - remove previous comments

--- clojure/log-levels/src/log_levels.clj
+++ clojure/log-levels/src/log_levels.clj
@@ -5,11 +5,10 @@
   "Takes a string representing a log line
    and returns its message with whitespace trimmed."
   [s]
-  ;; (re-find #"\[WARNING\]: " s)
-  ;; (last (str/split s, #"\[WARNING\]: "))
-  ;; (last (str/split s, #"\[ERROR\]: "))
-  ;; (last (str/split s, #"\[INFO\]: "))
-  (clojure.string/trimr (last (str/split s, #"\[\w+\]:\s*")))
+
+  (->> (str/split s #"\[\w+\]:\s*")
+       (last)
+       (clojure.string/trimr))
   )
 
 (defn log-level
