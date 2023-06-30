03947b87019af7deb31f2c3e47bb7a9f7d9940be
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-06-30 21:07:33 +0200

    Add Clojure track solution for Cars, Assemble! iteration#1.0 - submit the solution

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,12 +1,28 @@
 (ns cars-assemble)
 
+(defn flawless_rate
+  []
+  221.0
+  )
+
+(defn success_rate
+  [speed]
+  (cond
+    (< speed 5) 1.0
+    (< speed 9) 0.9
+    (= speed 9) 0.8
+    :else 0.77)
+  )
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
+  (* (* speed (flawless_rate)) (success_rate speed))
   )
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
+  (int (/ (production-rate speed) 60))
   )
