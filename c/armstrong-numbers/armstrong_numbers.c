cd1e276a19da3543e5a73712d927f0ea145cb3b6
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-07-28 19:49:59 +0200

    Add C track solution for Armstrong Numbers iteration#3.0 - remove printf-s and the necessary include - rename can consolidate variables to be more sensible - submit the 3rd iteration of the solution

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,39 +1,27 @@
 #include "armstrong_numbers.h"
-#include <stdio.h>
 
 int calculate_armstrong_from(int raw_number);
 
 int calculate_armstrong_from(int raw_number)
 {
-  int remainder = raw_number;
-  int sum = 0;
-  int digits_count = 0;
-  int i = 0;
-  int all_digits[10] = {0};
+  int all_digits[100] = {0};
   int armstrong_sum = 0;
-  printf("\nBuilding the next example for %d\n", raw_number);
-  for(i = 0; remainder > 0; i++)
+  int digits_count;
+  int leftover_number = raw_number;
+  for(digits_count = 0; leftover_number > 0; digits_count++)
   {
-    int last_digit = remainder % 10;
-    int division_result = remainder / 10;
-    printf("line %d, remainder before: %d, after: %d\n", i+1, remainder, division_result);
-    remainder = division_result;
-    sum += last_digit;
-    all_digits[i] = last_digit;
-    digits_count++;
+    all_digits[digits_count] = leftover_number % 10;
+    leftover_number = leftover_number / 10;
   }
-  printf("digits_count is %d, i is %d\n", digits_count, i);
-  printf("sum is %d\n", sum);
   for(int j = 0; j < digits_count; j++)
   {
     int multiplication_result = 1;
-    for(int k = 0; k < digits_count; k++)
+    for(int i = 0; i < digits_count; i++)
     {
       multiplication_result *= all_digits[j];
     }
     armstrong_sum += multiplication_result;
   }
-  printf("armstrong_sum is %d\n", armstrong_sum);
   return armstrong_sum;
 }
 
