3566699fbc9b6bebc5377c9d947b52dec82f9b53
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-07-30 22:07:00 +0200

    Add C track solution for Reverse String iteration#1.0 - add the minimal compilable code - fail all tests - submit the "solution"

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,1 +1,12 @@
 #include "reverse_string.h"
+#include <stdio.h>
+
+char *reverse(const char *value)
+{
+  printf("%s\n", value);
+  char* reversed[100] = {""};
+  reversed[0] = "O";
+  reversed[1] = "\0";
+
+  return *reversed;
+}
