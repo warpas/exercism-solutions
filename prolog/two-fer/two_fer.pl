2a8865d5a04c4dcf4eda55b9f162725d097f0f1c
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-07-30 14:59:17 +0200

    Add Prolog track solution for Two-Fer iteration#1.0 - submit the solution

--- prolog/two-fer/two_fer.pl
+++ prolog/two-fer/two_fer.pl
@@ -1,3 +1,4 @@
-two_fer(Name, Dialogue).
+two_fer(Input, Dialogue) :-
+  interpolate_string("One for {Name}, one for me.", Dialogue, [Name=Input], []), !.
 
-two_fer(Dialogue).
+two_fer("One for you, one for me.").
