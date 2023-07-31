ab479cce831a0860737fa5ee15f8f87af477a9b2
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-07-31 18:00:13 +0200

    Add C track solution for Reverse String iteration#2.0 - remove printf-s - move the guard clause for empty strings to the top of the `reverse` function - rename `revved` to `reversed_string` - submit the 2nd iteration of the solution

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -6,39 +6,7 @@
 char *reverse(const char *value)
 {
   int tab[100] = {0};
-
-  printf("Input: %s\n", value);
   int length = strlen(value);
-  printf("Length: %d\n", length);
-  printf("Tab[0]: %d\n", tab[0]);
-  char revved[100] = {0};
-  int i = 0;
-
-  for(; i < length; i++)
-  {
-    int char_code = (int)value[i];
-    tab[i] = (int)value[length-i-1];
-    printf("value[%d]: %c %d %d, tab[i] = %d\n", i, value[i], value[i], char_code, tab[i]);
-  }
-  printf("i: %d\n", i);
-  int j=0;
-  for(; j < length; j++)
-  {
-    printf("Js tab: %d\n", tab[j]);
-    revved[j] = (char)tab[j];
-    printf("Js revved: %c\n", revved[j]);
-  }
-  revved[j] = '\0';
-//   char* whole_string = revved;
-//   printf("Are we done? Whole: %s\n", whole_string);
-
-  char* whole_string = malloc(length + 1);
-
-  if(whole_string == NULL)
-  {
-    return NULL;
-  }
-  strcpy(whole_string, revved);
 
   if(length == 0)
   {
@@ -47,5 +15,21 @@ char *reverse(const char *value)
     return default_value;
   }
 
+  char reversed_string[100] = {0};
+  int i = 0;
+
+  for(; i < length; i++)
+  {
+    tab[i] = (int)value[length-i-1];
+  }
+  int j=0;
+  for(; j < length; j++)
+  {
+    reversed_string[j] = (char)tab[j];
+  }
+  reversed_string[j] = '\0';
+
+  char* whole_string = malloc(length + 1);
+  strcpy(whole_string, reversed_string);
   return whole_string;
 }
