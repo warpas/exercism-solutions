beaaa33a040ade2177f84cce116a50441c7cde94
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-07-31 08:27:03 +0200

    Add C track solution for Reverse String iteration#1.1 - struggle with creating an empty string, unsuccessfully due to malloc errors - copy the definition for `strdup` from the internet to make an empty string - pass the first test

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,12 +1,33 @@
 #include "reverse_string.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *internet_strdup(const char *s);
 
 char *reverse(const char *value)
 {
-  printf("%s\n", value);
+  printf("Input: %s\n", value);
+  int length = strlen(value);
+  printf("Length: %d\n", length);
   char* reversed[100] = {""};
   reversed[0] = "O";
   reversed[1] = "\0";
+  if(length == 0)
+  {
+    char* default_value = internet_strdup(value);
+    return default_value;
+  }
 
   return *reversed;
+}
+
+char *internet_strdup(const char *s)
+{
+    size_t size = strlen(s) + 1;
+    char *p = malloc(size);
+    if (p != NULL) {
+        memcpy(p, s, size);
+    }
+    return p;
 }
