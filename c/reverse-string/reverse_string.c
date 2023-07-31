e6f9f709ffaa15846bab4b5df51490fe956f96b4
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-07-31 17:48:40 +0200

    Add C track solution for Reverse String iteration#1.2 - attempt `int` conversion and then converting back to `char` - plenty of malloc errors throughout - TIL copying strings in C is not obvious to me - TIL learn to use malloc while declaring char pointers - get rid of `internet_strdup` and replace it with `malloc` + `strcpy` - use the `malloc` + `strcpy` syntax to copy the reversed string for return - not sure if `strcpy` will work on Exercism - pass all of the tests

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -3,31 +3,49 @@
 #include <string.h>
 #include <stdlib.h>
 
-char *internet_strdup(const char *s);
-
 char *reverse(const char *value)
 {
+  int tab[100] = {0};
+
   printf("Input: %s\n", value);
   int length = strlen(value);
   printf("Length: %d\n", length);
-  char* reversed[100] = {""};
-  reversed[0] = "O";
-  reversed[1] = "\0";
+  printf("Tab[0]: %d\n", tab[0]);
+  char revved[100] = {0};
+  int i = 0;
+
+  for(; i < length; i++)
+  {
+    int char_code = (int)value[i];
+    tab[i] = (int)value[length-i-1];
+    printf("value[%d]: %c %d %d, tab[i] = %d\n", i, value[i], value[i], char_code, tab[i]);
+  }
+  printf("i: %d\n", i);
+  int j=0;
+  for(; j < length; j++)
+  {
+    printf("Js tab: %d\n", tab[j]);
+    revved[j] = (char)tab[j];
+    printf("Js revved: %c\n", revved[j]);
+  }
+  revved[j] = '\0';
+//   char* whole_string = revved;
+//   printf("Are we done? Whole: %s\n", whole_string);
+
+  char* whole_string = malloc(length + 1);
+
+  if(whole_string == NULL)
+  {
+    return NULL;
+  }
+  strcpy(whole_string, revved);
+
   if(length == 0)
   {
-    char* default_value = internet_strdup(value);
+    char* default_value = malloc(length + 1);
+    strcpy(default_value, "");
     return default_value;
   }
 
-  return *reversed;
-}
-
-char *internet_strdup(const char *s)
-{
-    size_t size = strlen(s) + 1;
-    char *p = malloc(size);
-    if (p != NULL) {
-        memcpy(p, s, size);
-    }
-    return p;
+  return whole_string;
 }
