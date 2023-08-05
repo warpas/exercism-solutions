fb00d49f031a681a979986f045d39048f446b23e
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-08-05 14:10:47 +0200

    Add Swift track solution for Layers of Lasagna iteration#1.0 - TIL defining constanst is done by `let expectedMinutesInOven = 40` - TIL defining functions is done by `func remainingMinutesInOven(elapsedMinutes: Int) -> Int {}` - submit the solution

--- swift/lasagna/Sources/Lasagna/Lasagna.swift
+++ swift/lasagna/Sources/Lasagna/Lasagna.swift
@@ -1,7 +1,13 @@
-// TODO: define the 'expectedMinutesInOven' constant
+let expectedMinutesInOven = 40
 
-// TODO: define the 'remainingMinutesInOven' function
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+  return 40 - elapsedMinutes
+}
 
-// TODO: define the 'preparationTimeInMinutes' function
+func preparationTimeInMinutes(layers: Int) -> Int {
+  return layers * 2
+}
 
-// TODO: define the 'totalTimeInMinutes' function
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+  return preparationTimeInMinutes(layers: layers) + elapsedMinutes
+}
