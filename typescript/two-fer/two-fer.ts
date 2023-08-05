87981f75087c0ab4d68aec8d61f7342d17d0ca3a
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-08-05 15:07:49 +0200

    Add TypeScript track solution for Two Fer iteration#1.0 - TIL default argument values in TS is done by `function twoFer(name: string = "you")` - submit the solution

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -1,13 +1,3 @@
-/**
- * This stub is provided to make it straightforward to get started.
- */
-
-export function twoFer(): string {
-  // ^                 ^   ^ this is called a return type; it's the type of the
-  // ^                 ^     value that is returned from this function
-  // ^                 ^
-  // ^                 parameters go here
-  // ^
-  // allows the tests to import this function and call it
-  // <-- Your code goes here. You may remove all the commentary in this file.
+export function twoFer(name: string = "you"): string {
+  return `One for ${name}, one for me.`;
 }
