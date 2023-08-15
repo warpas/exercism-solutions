b30d13d3d2c7a5109ef983218a81923cd408b02b
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-08-15 16:44:52 +0200

    Add Swift track solution for Leap iteration#1.0 - TIL constructor is done by `init(calendarYear: Int) {` in Swift - TIL I need to declare a property before assigning to it (`let isLeapYear: Bool`) - submit the solution

--- swift/leap/Sources/Leap/Leap.swift
+++ swift/leap/Sources/Leap/Leap.swift
@@ -1,3 +1,9 @@
 class Year {
-  // Write your code for the 'Leap' exercise in this file.
+  let isLeapYear: Bool
+
+  init(calendarYear: Int) {
+    self.isLeapYear = calendarYear % 400 == 0
+      || (calendarYear % 4 == 0 && calendarYear % 100 != 0)
+
+  }
 }
