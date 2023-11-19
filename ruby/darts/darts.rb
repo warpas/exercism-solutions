a7e07a9ca3d91a22d955a2843eb799cca5c09b53
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-11-19 09:43:24 +0100

    Add Ruby track solution for Darts iteration#0.0 - add the exercise - add the class and few methods to pass the first test

---
+++ ruby/darts/darts.rb
@@ -0,0 +1,15 @@
+=begin
+Write your code for the 'Darts' exercise in this file. Make the tests in
+`darts_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/darts` directory.
+=end
+class Darts
+  def initialize(x, y)
+  end
+
+  def score
+    0
+  end
+end
