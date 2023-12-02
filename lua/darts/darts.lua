a40acf1b55285bc190c77e8cf81f82ab8daf93ae
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-02 19:20:38 +0100

    Add Lua track solution for Darts iteration#1.0 - port the Ruby track solution to Lua - submit the solution

--- lua/darts/darts.lua
+++ lua/darts/darts.lua
@@ -1,7 +1,22 @@
 local Darts = {}
 
 function Darts.score(x, y)
+  boundary = x * x + y * y
 
+  if(boundary > 100)
+  then
+    return 0
+  end
+  if(boundary > 25)
+  then
+    return 1
+  end
+  if(boundary > 1)
+  then
+    return 5
+  end
+
+  return 10
 end
 
 return Darts
