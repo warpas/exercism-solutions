37119932149e36f157cbf7bc4091bf94a95d4d01
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-02 18:50:22 +0100

    Add Lua track solution for Difference of Squares iteration#1.0 - TIL: variable definition with dynamic types is possible `sum = 0` - TIL: how to do a simple while loop - pass all tests - submit the solution

--- lua/difference-of-squares/difference-of-squares.lua
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -1,13 +1,27 @@
 local function square_of_sum(n)
-
+  sum = 0
+  current = n
+  while(current > 0)
+  do
+    sum = sum + current
+    current = current - 1
+  end
+  return sum * sum
 end
 
 local function sum_of_squares(n)
-
+  sum = 0
+  current = n
+  while(current > 0)
+  do
+    sum = sum + current * current
+    current = current - 1
+  end
+  return sum
 end
 
 local function difference_of_squares(n)
-
+  return square_of_sum(n) - sum_of_squares(n)
 end
 
 return {
