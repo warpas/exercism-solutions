45346f6cfbb4dc2b32e780ce7b3a92bdbcde4638
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-02 19:09:13 +0100

    Add Lua track solution for Collatz Conjecture iteration#1.0 - TIL: conditional statements in Lua `if(bool) ... then ... else ... end` - TIL: comments in Lua `-- comment` - TIL: modulus is Lua `math.fmod(n, divisor)` - TIL: throw error in Lua by `error(message)` - submit the solution

--- lua/collatz-conjecture/collatz-conjecture.lua
+++ lua/collatz-conjecture/collatz-conjecture.lua
@@ -1,3 +1,20 @@
 return function(n)
+  if(n < 1)
+  then
+    error("Only positive numbers are allowed")
+  end
 
+  total_steps = 0
+  while(n > 1)
+  do
+    number_even = (math.fmod(n, 2) == 0)
+    if(number_even)
+    then
+      n = n / 2
+    else
+      n = (n * 3 + 1)
+    end
+    total_steps = total_steps + 1
+  end
+  return total_steps
 end
