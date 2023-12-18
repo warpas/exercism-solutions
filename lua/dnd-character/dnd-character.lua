012b1f23b5625d7b7f6b7ac23ddb88dff961980d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-18 08:23:04 +0100

    Add Lua track solution for Dnd Character iteration#1.0 - fix the min condition - TIL how to create a new object in Lua - submit the solution

--- lua/dnd-character/dnd-character.lua
+++ lua/dnd-character/dnd-character.lua
@@ -1,12 +1,12 @@
 local Character = {}
 
 local function ability(scores)
-  min = 0
+  min = 20
   sum = 0
 
   for i = 1, 4 do
     current = scores[i]
-    if min < current then
+    if (min > current) then
       min = current
     end
     sum = sum + current
@@ -25,6 +25,21 @@ end
 
 local function modifier(input)
   return math.floor((input - 10) / 2)
+end
+
+function Character:new(name)
+  constitution_value = ability(roll_dice())
+  characterObject = {
+    name = name,
+    strength = ability(roll_dice()),
+    dexterity = ability(roll_dice()),
+    constitution = constitution_value,
+    intelligence = ability(roll_dice()),
+    wisdom = ability(roll_dice()),
+    charisma = ability(roll_dice()),
+    hitpoints = 10 + modifier(constitution_value)
+  }
+  return characterObject
 end
 
 return {
