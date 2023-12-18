2c3c3c8c5b47c8085f9a6d0aff6b7c801aab77fa
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-18 08:06:56 +0100

    Add Lua track solution for Dnd Character iteration#0.1 - write a naive solutions for the pre-defined functions - pass the first 19 tests

--- lua/dnd-character/dnd-character.lua
+++ lua/dnd-character/dnd-character.lua
@@ -1,15 +1,30 @@
 local Character = {}
 
 local function ability(scores)
+  min = 0
+  sum = 0
 
+  for i = 1, 4 do
+    current = scores[i]
+    if min < current then
+      min = current
+    end
+    sum = sum + current
+  end
+  return sum - min
 end
 
 local function roll_dice()
+  a = math.random(1,6)
+  b = math.random(1,6)
+  c = math.random(1,6)
+  d = math.random(1,6)
 
+  return {a, b, c, d}
 end
 
 local function modifier(input)
-
+  return math.floor((input - 10) / 2)
 end
 
 return {
