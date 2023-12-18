fdc23d6ce986d013bc41044f41a015f2811b3e7d
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-18 08:36:32 +0100

    Add TypeScript track solution for Leap iteration#1.0 - submit the solution

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,6 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+export function isLeap(year: number) : boolean {
+  const divisibleBy4 = year % 4 === 0
+  const notDivisibleBy100 = year % 100 !== 0
+  const divisibleBy400 = year % 400 === 0
+  return divisibleBy400 || (divisibleBy4 && notDivisibleBy100)
 }
