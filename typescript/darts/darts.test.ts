a1922b840605d381d16b8cf87c6a491bc32afecc
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-18 09:26:04 +0100

    Add TypeScript track solution for Darts iteration#1.0 - submit the solution

--- typescript/darts/darts.test.ts
+++ typescript/darts/darts.test.ts
@@ -5,51 +5,51 @@ describe('Darts', () => {
     expect(score(-9, 9)).toEqual(0)
   })
 
-  xit('On the outer circle', () => {
+  it('On the outer circle', () => {
     expect(score(0, 10)).toEqual(1)
   })
 
-  xit('On the middle circle', () => {
+  it('On the middle circle', () => {
     expect(score(-5, 0)).toEqual(5)
   })
 
-  xit('On the inner circle', () => {
+  it('On the inner circle', () => {
     expect(score(0, -1)).toEqual(10)
   })
 
-  xit('Exactly on centre', () => {
+  it('Exactly on centre', () => {
     expect(score(0, 0)).toEqual(10)
   })
 
-  xit('Near the centre', () => {
+  it('Near the centre', () => {
     expect(score(-0.1, -0.1)).toEqual(10)
   })
 
-  xit('Just within the inner circle', () => {
+  it('Just within the inner circle', () => {
     expect(score(0.7, 0.7)).toEqual(10)
   })
 
-  xit('Just outside the inner circle', () => {
+  it('Just outside the inner circle', () => {
     expect(score(0.8, -0.8)).toEqual(5)
   })
 
-  xit('Just within the middle circle', () => {
+  it('Just within the middle circle', () => {
     expect(score(-3.5, 3.5)).toEqual(5)
   })
 
-  xit('Just outside the middle circle', () => {
+  it('Just outside the middle circle', () => {
     expect(score(-3.6, -3.6)).toEqual(1)
   })
 
-  xit('Just within the outer circle', () => {
+  it('Just within the outer circle', () => {
     expect(score(-7.0, 7.0)).toEqual(1)
   })
 
-  xit('Just outside the outer circle', () => {
+  it('Just outside the outer circle', () => {
     expect(score(7.1, -7.1)).toEqual(0)
   })
 
-  xit('Asymmetric position between the inner and middle circles', () => {
+  it('Asymmetric position between the inner and middle circles', () => {
     expect(score(0.5, -4)).toEqual(5)
   })
 })
--- typescript/darts/darts.ts
+++ typescript/darts/darts.ts
@@ -1,3 +1,11 @@
-export function score(x: unknown, y: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function score(x: number, y: number): number {
+  const x_abs: number = Math.abs(x);
+  const y_abs: number = Math.abs(y);
+  const boundary: number = x_abs * x_abs + y_abs * y_abs;
+
+  if(boundary > 100) { return 0 }
+  if(boundary > 25) { return 1 }
+  if(boundary > 1) { return 5 }
+
+  return 10
 }
