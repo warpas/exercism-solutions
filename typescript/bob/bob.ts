0ab1c9335bddc3e9013f319c6ab5e824f5140524
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-21 20:20:11 +0100

    Add TypeScript track solution for Bob iteration#1.0 -  submit the solution

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,3 +1,25 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function hey(message: string): string {
+  if(shouting(message) && question(message)) {
+    return "Calm down, I know what I'm doing!"
+  } else if(silence(message)) {
+    return 'Fine. Be that way!'
+  } else if(shouting(message)) {
+    return 'Whoa, chill out!'
+  } else if(question(message)) {
+    return 'Sure.'
+  }
+
+  return 'Whatever.'
 }
+
+const question = (message: string): boolean => (
+  message.trim().slice(-1) === "?"
+)
+
+const shouting = (message: string): boolean => (
+  message === message.toUpperCase() && message !== message.toLowerCase()
+)
+
+const silence = (message: string): boolean => (
+  !message.trim()
+)
