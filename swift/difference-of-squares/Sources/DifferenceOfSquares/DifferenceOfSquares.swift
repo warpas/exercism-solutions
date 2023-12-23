15867ccc1f3e8cbbe002e2a045d1869314c14bd4
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2023-12-23 18:19:15 +0100

    Add Swift track solution for Difference of Squares iteration#2.0 - change `var`s to consts where possible - consolidate calculations to a single loop - remove `number` from class fields - submit the 2nd iteration of the solution

--- swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -1,23 +1,14 @@
 class Squares {
-  var number: Int
-  var sumOfSquares: Int
-  var squareOfSum: Int
-  var differenceOfSquares: Int
+  let sumOfSquares: Int
+  let squareOfSum: Int
+  let differenceOfSquares: Int
 
   init(_ number: Int) {
-    self.number = number
-
-    // print("\n number: \(number)")
     var sum: Int = 0
-    for index in 1...self.number {
-      // print("\n index: \(index)")
-      sum = sum + index
-    }
-    // print("\n sum: \(sum)")
-
     var sumOfSquares: Int = 0
-    for index in 1...self.number {
-      sumOfSquares = sumOfSquares + index * index
+    for index in 1...number {
+      sum += index
+      sumOfSquares += index * index
     }
 
     self.squareOfSum = sum * sum
