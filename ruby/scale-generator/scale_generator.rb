87f04b89d34a9001b9f3df0ffac573c5a2d51ed4
Dawid Warpas-Sulka <dawid.warpas@gmail.com>
2025-03-22 22:23:59 +0200

    Add Ruby track solution for Scale Generator iteration#0.4.2 - align indentation after removing the alternate execution path - remove commented out code - add a TODO for variable names - remove the top level comment about task worthiness

--- ruby/scale-generator/scale_generator.rb
+++ ruby/scale-generator/scale_generator.rb
@@ -1,10 +1,10 @@
 class Scale
-  # I deem this task unworthy of my time
   CHROMATIC_SHARP_SCALE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
   CHROMATIC_FLAT_SCALE = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
 
   def initialize(tonic, intervals, steps='')
     @tonic = tonic.upcase
+    # TODO: intervals should be renamed to something more descriptive
     @intervals = intervals
     @steps = steps
   end
@@ -20,30 +20,22 @@ class Scale
     starting_index = scale.index(@tonic)
     result = []
 
-    # if @steps.empty?
-    #   0.upto(11) do |num|
-    #     current_index = (num + starting_index) % 12
-    #     new_element = scale[current_index]
-    #     result << new_element
-    #   end
-    # else
-      # puts ''
-      # p @steps unless @steps.empty?
-      next_step = 0
-      current_step = 0
-      last_step = starting_index
+    # puts ''
+    # p @steps unless @steps.empty?
+    next_step = 0
+    current_step = 0
+    last_step = starting_index
+    current_index = (last_step + next_step) % 12
+    new_element = scale[current_index]
+    while(!result.include?(new_element)) do
+      # p "new_element: #{new_element}, current_index: #{current_index}" unless @steps.empty?
+      last_step += next_step
+      next_step = @steps[current_step] == 'M' ? 2 : 1
+      current_step += 1
+      result << new_element
       current_index = (last_step + next_step) % 12
       new_element = scale[current_index]
-      while(!result.include?(new_element)) do
-        # p "new_element: #{new_element}, current_index: #{current_index}" unless @steps.empty?
-        last_step += next_step
-        next_step = @steps[current_step] == 'M' ? 2 : 1
-        current_step += 1
-        result << new_element
-        current_index = (last_step + next_step) % 12
-        new_element = scale[current_index]
-      end
-    # end
+    end
 
     result
   end
